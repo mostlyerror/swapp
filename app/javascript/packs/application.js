6750bb44e4a240fec8e92a96ec36484da53a93c8
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-12 14:33:45 -0700

    added header, footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "stylesheets/application"
+import "stylesheets/application.scss"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
