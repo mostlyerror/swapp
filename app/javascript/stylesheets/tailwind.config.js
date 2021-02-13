f216221060dc50a2b28e1f51222a118853bcd95a
Ben Poon <benjamintpoon@gmail.com>
2021-02-13 00:34:21 -0700

    Red Flag Form & Results (#4)
    * extract some button utilities
    
    * add some styles to the form
    
    * swtich to a black bg, match form input heights in search form
    
    * adjust size and spacing of search form
    
    * extending outline utility with `outline-black` and extracting an
    outlined button component, `btn-outline`
    
    * Adjusting help text styling and CTA
    
    * improving whitespace in red flag results table

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  purge: [
-    "./app/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/javascript/**/*.vue",
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: [
+		"./app/**/*.html.erb",
+		"./app/helpers/**/*.rb",
+		"./app/javascript/**/*.js",
+		"./app/javascript/**/*.vue",
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			outline: {
+				black: '2px solid black',
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
