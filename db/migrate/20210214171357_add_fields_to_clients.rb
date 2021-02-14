ca3c5e1b3ee23606ca6762f16b335d73af5b5b2b
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-14 10:29:40 -0700

    add demographic fields to client record, email, phone

---
+++ db/migrate/20210214171357_add_fields_to_clients.rb
@@ -0,0 +1,8 @@
+class AddFieldsToClients < ActiveRecord::Migration[6.0]
+  def change
+    add_column :clients, :race, :string
+    add_column :clients, :ethnicity, :string
+    add_column :clients, :email_address, :string
+    add_column :clients, :phone_number, :string
+  end
+end
