93fbadce770427a8557ab1fa7244cc19f922d17a
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-14 13:24:22 -0700

    add transaction around seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,152 +6,166 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([
-  {
-    email: 'bpoon@codeforamerica.org',
-    first_name: 'Ben',
-    last_name: 'Poon',
-    password: 'password',
-    password_confirmation: 'password',
-    admin: true,
-  },
-  {
-    email: 'ftang@codeforamerica.org',
-    first_name: 'Fiona',
-    last_name: 'Tang',
-    password: 'password',
-    password_confirmation: 'password',
-    admin: true,
-  },
-  {
-    email: 'bjohnson@codeforamerica.org',
-    first_name: 'Brandon',
-    last_name: 'Johnson',
-    password: 'password',
-    password_confirmation: 'password',
-    admin: true,
-  },
-  {
-    email: 'ashleydunn@almosthome.org',
-    first_name: 'Ashley',
-    last_name: 'Dunn',
-    password: 'password',
-    password_confirmation: 'password',
-    admin: true,
-  }
-])
+ActiveRecord::Base.transaction do |t|
 
-intake_users = User.create([
-  {
-    email: 'heather@adcogov.org',
-    first_name: 'Heather',
-    last_name: 'McClure',
-    password: 'password',
-    password_confirmation: 'password',
-  },
-  {
-    email: 'myra@cityofthornton.org',
-    first_name: 'Myra',
-    last_name: 'Someone',
-    password: 'password',
-    password_confirmation: 'password',
-  },
-  {
-    email: 'mario@cityofthornton.org',
-    first_name: 'Mario',
-    last_name: 'Someone',
-    password: 'password',
-    password_confirmation: 'password',
-  },
-  {
-    email: 'jessica@cityofnorthglenn.org',
-    first_name: 'Jessica',
-    last_name: 'Someone',
-    password: 'password',
-    password_confirmation: 'password',
-  }
-])
-
-motels = Motel.create!([
-  {
-    name: 'Quality Inn Denver Westminster',
-    address: {
-      street: '8500 Turnpike Dr',
-      city:'Westminster',
-      state: 'CO', 
-      ip: 80030
+  User.create([
+    {
+      email: 'bpoon@codeforamerica.org',
+      first_name: 'Ben',
+      last_name: 'Poon',
+      password: 'password',
+      password_confirmation: 'password',
+      admin: true,
     },
-    phone: '+13034283333'
-  },
-  {
-    name: 'Comfort Inn & Suites Denver Northeast Brighton',
-    address: {
-      street: '2180 South Medical Center Dr', 
-      city: 'Brighton',
-      state: 'CO',
-      zip: 80601
+    {
+      email: 'ftang@codeforamerica.org',
+      first_name: 'Fiona',
+      last_name: 'Tang',
+      password: 'password',
+      password_confirmation: 'password',
+      admin: true,
     },
-    phone: '+17206851500'
-  }
-])
-
-motel_users = User.create!([
-  {
-    email: 'carol@comfortinn.com',
-    first_name: 'Carol',
-    last_name: 'Motel',
-    password: 'password',
-    password_confirmation: 'password',
-  },
-  {
-    email: 'harriet@hometownestudios.com',
-    first_name: 'Harriett',
-    last_name: 'Motel',
-    password: 'password',
-    password_confirmation: 'password',
-  },
-  {
-    email: 'quincey@qualityinn.com',
-    first_name: 'Quincey',
-    last_name: 'Motel',
-    password: 'password',
-    password_confirmation: 'password',
-  }
-])
-
-clients = []
-50.times do
-  client = Client.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    date_of_birth: Faker::Date.birthday,
-    gender: Client::GENDER.sample,
-    race: Client::RACE.sample,
-    ethnicity: Client::ETHNICITY.sample,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    email_address: Faker::Internet.email,
-  )
-  clients << client
-end
-
-20.times do
-  IncidentReport.create!(
-    client: clients.sample,
-    reporter_id: motel_users.sample.id,
-    occurred_at: Faker::Date.between(from: 60.days.ago, to: Date.today),
-    description: Faker::Quote.jack_handey,
-  )
-end
-
-10.times do
-  client = clients.sample
-  user = intake_users.sample
-  Intake.create!(
-    client: client,
-    user: user,
-    survey: {
-      king_soopers_card: ['yes', 'no'].sample,
-      bus_pass: ['yes', 'no'].sample,
+    {
+      email: 'bjohnson@codeforamerica.org',
+      first_name: 'Brandon',
+      last_name: 'Johnson',
+      password: 'password',
+      password_confirmation: 'password',
+      admin: true,
+    },
+    {
+      email: 'ashleydunn@almosthome.org',
+      first_name: 'Ashley',
+      last_name: 'Dunn',
+      password: 'password',
+      password_confirmation: 'password',
+      admin: true,
     }
-  )
+  ])
+
+  intake_users = User.create([
+    {
+      email: 'heather@adcogov.org',
+      first_name: 'Heather',
+      last_name: 'McClure',
+      password: 'password',
+      password_confirmation: 'password',
+    },
+    {
+      email: 'myra@cityofthornton.org',
+      first_name: 'Myra',
+      last_name: 'Someone',
+      password: 'password',
+      password_confirmation: 'password',
+    },
+    {
+      email: 'mario@cityofthornton.org',
+      first_name: 'Mario',
+      last_name: 'Someone',
+      password: 'password',
+      password_confirmation: 'password',
+    },
+    {
+      email: 'jessica@cityofnorthglenn.org',
+      first_name: 'Jessica',
+      last_name: 'Someone',
+      password: 'password',
+      password_confirmation: 'password',
+    }
+  ])
+
+  motels = Motel.create!([
+    {
+      name: 'Quality Inn Denver Westminster',
+      address: {
+        street: '8500 Turnpike Dr',
+        city:'Westminster',
+        state: 'CO', 
+        ip: 80030
+      },
+      phone: '+13034283333'
+    },
+    {
+      name: 'Comfort Inn & Suites Denver Northeast Brighton',
+      address: {
+        street: '2180 South Medical Center Dr', 
+        city: 'Brighton',
+        state: 'CO',
+        zip: 80601
+      },
+      phone: '+17206851500'
+    }
+  ])
+
+  motel_users = User.create!([
+    {
+      email: 'carol@comfortinn.com',
+      first_name: 'Carol',
+      last_name: 'Motel',
+      password: 'password',
+      password_confirmation: 'password',
+    },
+    {
+      email: 'harriet@hometownestudios.com',
+      first_name: 'Harriett',
+      last_name: 'Motel',
+      password: 'password',
+      password_confirmation: 'password',
+    },
+    {
+      email: 'quincey@qualityinn.com',
+      first_name: 'Quincey',
+      last_name: 'Motel',
+      password: 'password',
+      password_confirmation: 'password',
+    }
+  ])
+
+  clients = []
+  50.times do
+    client = Client.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      date_of_birth: Faker::Date.birthday,
+      gender: Client::GENDER.sample,
+      race: Client::RACE.sample,
+      ethnicity: Client::ETHNICITY.sample,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      email_address: Faker::Internet.email,
+    )
+    clients << client
+  end
+
+  20.times do
+    IncidentReport.create!(
+      client: clients.sample,
+      reporter_id: motel_users.sample.id,
+      occurred_at: Faker::Date.between(from: 60.days.ago, to: Date.today),
+      description: Faker::Quote.jack_handey,
+    )
+  end
+
+  10.times do
+    client = clients.sample
+    user = intake_users.sample
+    motel = motels.sample
+
+    intake = Intake.create!(
+      client: client,
+      user: user,
+      survey: {
+        king_soopers_card: ['yes', 'no'].sample,
+        bus_pass: ['yes', 'no'].sample,
+      }
+    )
+
+    Voucher.create!(
+      client: client,
+      user: user,
+      motel: motel,
+      start_date: Date.today,
+      end_date: Date.today + 2.days,
+      number: 'AH123',
+    )
+  end
 end
