e6a7705a1500879fd4225b12da3c69f21d53de62
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-13 23:36:14 -0700

    setting up all select values

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,27 @@ class Client < ApplicationRecord
   validates_presence_of :first_name, :last_name, :date_of_birth, :gender
 
   has_many :incident_reports
+
+  GENDER = [
+    "Female",
+    "Male",
+    "Trans Female (MTF)",
+    "Trans Male (FTM)",
+    "Gender Non-conforming",
+    "Client Doesn't Know",
+    "Client Refused",
+  ]
+
+  RACE = [
+    "American Indian or Alaskan Native",
+    "Asian",
+    "Black or African American",
+    "Native Hawaiian or Other Pacific Islander",
+    "White",
+  ]
+
+  ETHNICITY = [
+    "Hispanic or Latino",
+    "Not Hispanic or Latino",
+  ]
 end
