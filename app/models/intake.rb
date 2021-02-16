93b3764956af3706180f0346021685be39a37969
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-16 12:52:39 -0700

    trim whitespace around survey answers before saving

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -1,6 +1,14 @@
 class Intake < ApplicationRecord
   belongs_to :client
   accepts_nested_attributes_for :client
-
   belongs_to :user
+  before_save :trim_survey_fields
+
+  private 
+
+    def trim_survey_fields
+      survey.keys.each do |key|
+        survey[key] = survey[key].strip
+      end
+    end
 end
