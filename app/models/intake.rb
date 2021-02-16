ab8f350904ad51858714366d155d71753f3dca70
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-15 17:34:06 -0700

    remove method to auto-fail validation (used for testing)

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -1,13 +1,6 @@
 class Intake < ApplicationRecord
-  validate :asdf
-
   belongs_to :client
   accepts_nested_attributes_for :client
 
   belongs_to :user
-
-  def asdf
-    false
-    true
-  end
 end
