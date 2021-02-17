a00e230aeb3afcb138bff82e73426a7c078a4ed7
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-17 03:06:33 -0700

    setup factory bot and swap_period factory

---
+++ test/factories/swap_periods.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :swap_period do
+    trait :past do
+      start_date { 7.days.ago }
+      end_date { 4.days.ago }
+    end
+
+    trait :current do
+      start_date { 1.day.ago }
+      end_date { 1.day.from_now }
+    end
+
+    trait :future do
+      start_date { 1.day.from_now }
+      end_date { 4.days.from_now }
+    end
+  end
+end
