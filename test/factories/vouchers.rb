f2661e5bdd5884abf6ca34dfe66cf0ccd0125d54
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-18 14:49:52 -0700

    vouchers belong to swap period

--- test/factories/vouchers.rb
+++ test/factories/vouchers.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
-  factory :vouchers do
+  factory :voucher do
+    client
+    user
+    motel
+    swap_period
+    check_in { self.swap_period.start_date }
+    check_out { self.swap_period.end_date }
   end
 end
