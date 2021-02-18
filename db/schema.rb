2c6c35e5a1ec23608ca532f2e4a81da9c7fe3fb5
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-18 08:34:56 -0700

    add active bool to swap periods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_215306) do
+ActiveRecord::Schema.define(version: 2021_02_18_153406) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_215306) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
