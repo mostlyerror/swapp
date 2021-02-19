0d59294196b2d3994870c0b820a7d1c6a3b1cf42
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-19 04:11:11 -0700

    rendering form help messages based on relative intake day

--- app/models/swap_intake.rb
+++ app/models/swap_intake.rb
@@ -6,4 +6,20 @@ class SwapIntake
   def self.can_issue_voucher_today? swap
     can_issue_voucher_on? Date.today, swap
   end
+
+  def self.first_intake_day swap
+    swap.intake_period.first
+  end
+
+  def self.today_is_first_intake_day? swap
+    Date.today == first_intake_day(swap)
+  end
+
+  def self.last_intake_day swap
+    swap.intake_period.last
+  end
+
+  def self.today_is_last_intake_day? swap
+    Date.today == last_intake_day(swap)
+  end
 end
