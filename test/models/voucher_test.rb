76b194fd1137e421772a5b16608e87a64635fbcc
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-20 08:02:57 -0700

    test for voucher date ordering

--- test/models/voucher_test.rb
+++ test/models/voucher_test.rb
@@ -1,6 +1,21 @@
 require 'test_helper'
 
 class VoucherTest < ActiveSupport::TestCase
+  test "voucher dates must be in order" do
+    swap = build_stubbed(:swap_period, :current)
+    voucher = build_stubbed(:voucher, 
+      swap_period: swap,
+      check_in: swap.end_date,
+      check_out: swap.start_date
+    )
+    refute voucher.valid?
+    assert voucher.errors.key? :dates
+
+    voucher.check_in = swap.start_date
+    voucher.check_out = swap.end_date
+    assert voucher.valid?
+  end
+
   test "check_in/out only during swap period" do
     swap = build_stubbed(:swap_period, :current)
     voucher = build_stubbed(:voucher, 
