9fad29aa4d5571cdfc4c4c1545f4913116eb677d
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-20 13:40:42 -0700

    vouchers must have one night remaining at issuance

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -7,9 +7,26 @@ class Voucher < ApplicationRecord
   validates_presence_of :check_in, :check_out
   validates_uniqueness_of :client_id, scope: :swap_period_id
   validate :dates_must_be_today_or_later_when_issued, on: :create
-  validate :order_of_dates, :dates_must_fall_within_swap_period
+  validate :order_of_dates, :dates_must_fall_within_swap_period, :at_least_one_night_remaining
 
   after_create :save_number
+
+  def duration
+    ((check_out - check_in) + 1).to_i
+  end
+
+  def nights 
+    duration - 1
+  end
+
+  def nights_remaining
+    [(check_out -  Date.current).to_i, 0].max
+  end
+
+  def extend nights
+    self.check_out = self.check_out.to_date + nights.to_i.days
+    self
+  end
 
   private 
     def save_number
@@ -24,7 +41,7 @@ class Voucher < ApplicationRecord
     end
 
     def dates_must_be_today_or_later_when_issued
-      if (check_in && (check_in < Date.current)) or (check_out && (check_out < Date.current))
+      if (check_in < Date.current) or (check_out < Date.current)
         errors.add(:backdated, "Can't issue backdated voucher dates")
       end
     end
@@ -36,6 +53,12 @@ class Voucher < ApplicationRecord
 
       if swap_period && !(check_out.in? swap_period.swap_period)
         errors.add(:check_out, "check_out (#{check_out}) does not fall within swap period: #{swap_period.swap_period}")
+      end
+    end
+
+    def at_least_one_night_remaining
+      if nights_remaining < 1
+        return errors.add(:nights, "duration: (#{nights_remaining} nights remaining) must be for at least one night")
       end
     end
 end
--- test/models/voucher_test.rb
+++ test/models/voucher_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class VoucherTest < ActiveSupport::TestCase
+  test "voucher has to be for at least one night" do
+    swap = create(:swap_period,
+      start_date: Date.current.yesterday,
+      end_date: Date.current
+    )
+    assert_equal 1, swap.nights
+    assert_equal 0, swap.nights_remaining
+
+    voucher = build_stubbed(:voucher, swap_period: swap)
+    refute voucher.valid?
+    assert voucher.errors.key?(:nights)
+  end
+
   test "one voucher per period per client" do
     swap = create(:swap_period, :future)
     voucher = create(:voucher, swap_period: swap)
@@ -18,7 +31,7 @@ class VoucherTest < ActiveSupport::TestCase
   end
 
   test "voucher dates must be in order" do
-    swap = build_stubbed(:swap_period, :current)
+    swap = build_stubbed(:swap_period, :future)
     voucher = build_stubbed(:voucher, 
       swap_period: swap,
       check_in: swap.end_date,
@@ -39,6 +52,8 @@ class VoucherTest < ActiveSupport::TestCase
       check_in: swap.start_date,
       check_out: swap.end_date
     )
+    # this doesn't raise because the validation for dates being
+    # current or in the future only runs on creation, not save or build
     assert voucher.valid?
 
     voucher.check_in = swap.start_date - 1
@@ -50,15 +65,27 @@ class VoucherTest < ActiveSupport::TestCase
     assert voucher.errors.key? :check_out
   end
 
-  test "dates must be today or later on issuance" do
-    swap = build_stubbed(:swap_period, :past)
-    voucher = build(:voucher, 
-      swap_period: swap, 
-      check_in: swap.start_date, 
-      check_out: swap.end_date,
-    )
+  test "dates must be today or later at creation" do
+    swap = create(:swap_period, :past)
+    assert_raises do
+      create(:voucher, 
+        swap_period: swap, 
+        check_in: swap.start_date, 
+        check_out: swap.end_date,
+      )
+    end
 
-    refute voucher.valid?
-    assert voucher.errors.key? :backdated
+    swap = create(:swap_period, :current)
+    assert_raises do
+      create(:voucher, 
+        swap_period: swap, 
+        check_in: swap.start_date, 
+        check_out: swap.end_date,
+      )
+    end
+
+    assert_nothing_raised do
+      create(:voucher, swap_period: swap, check_in: Date.current)
+    end
   end
 end
