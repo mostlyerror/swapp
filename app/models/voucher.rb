6bdd97a615d0569c5ed49ea108908ddf96b79704
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-20 12:29:45 -0700

    validations for one voucher per client per period

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -4,6 +4,8 @@ class Voucher < ApplicationRecord
   belongs_to :motel
   belongs_to :swap_period
 
+  validates_presence_of :check_in, :check_out
+  validates_uniqueness_of :client_id, scope: :swap_period_id
   validate :dates_must_be_today_or_later_when_issued, on: :create
   validate :order_of_dates, :dates_must_fall_within_swap_period
 
@@ -16,23 +18,23 @@ class Voucher < ApplicationRecord
     end
 
     def order_of_dates
-      if check_out&.< check_in
+      if check_out && (check_out < check_in)
         errors.add(:dates, "check_out: #{check_out} must be same day or later than check_in: #{check_in}")
       end
     end
 
     def dates_must_be_today_or_later_when_issued
-      if (check_in < Date.current) or (check_out < Date.current)
+      if (check_in && (check_in < Date.current)) or (check_out && (check_out < Date.current))
         errors.add(:backdated, "Can't issue backdated voucher dates")
       end
     end
 
     def dates_must_fall_within_swap_period
-      if !check_in.in? swap_period.swap_period
+      if swap_period && !(check_in.in? swap_period.swap_period)
         errors.add(:check_in, "check_in (#{check_in}) does not fall within swap period: #{swap_period.swap_period}")
       end
 
-      if !check_out.in? swap_period.swap_period
+      if swap_period && !(check_out.in? swap_period.swap_period)
         errors.add(:check_out, "check_out (#{check_out}) does not fall within swap period: #{swap_period.swap_period}")
       end
     end
--- test/models/voucher_test.rb
+++ test/models/voucher_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class VoucherTest < ActiveSupport::TestCase
+  test "one voucher per period per client" do
+    swap = create(:swap_period, :future)
+    voucher = create(:voucher, swap_period: swap)
+    voucher = build(:voucher, 
+      swap_period: swap, 
+      client: voucher.client,
+      check_out: swap.start_date,
+      check_in: swap.start_date,
+      )
+    refute voucher.valid?
+    assert voucher.errors.key?(:client_id)
+
+    voucher.client = create(:client)
+    assert voucher.valid?
+  end
+
   test "voucher dates must be in order" do
     swap = build_stubbed(:swap_period, :current)
     voucher = build_stubbed(:voucher, 
