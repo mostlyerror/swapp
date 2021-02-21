0dfec464b9bf8f8049bab70ee967e9e9a3072d96
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-20 23:15:18 -0700

    big nono - rename a buncha migrations to match constant names from SwapPeriod -> Swap

--- db/migrate/20210217095830_create_swaps.rb
+++ db/migrate/20210217095830_create_swaps.rb
@@ -1,6 +1,6 @@
 class CreateSwaps < ActiveRecord::Migration[6.0]
   def change
-    create_table :swap_periods do |t|
+    create_table :swaps do |t|
       t.date :start_date, null: false
       t.date :end_date
       t.timestamps
