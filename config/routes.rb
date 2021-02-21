a65d90a8e03b14246fe614ffb898b36217604e3e
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-21 01:50:03 -0700

    replace date select with picker on intake form, fix redirect after intake form complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :clients
   resources :intakes
   resources :vouchers
-  get "vouchers/:id/created" => "vouchers#created"
+  get "vouchers/:id/created" => "vouchers#created", as: :voucher_created
   resources :swaps
 
   # get "admin" => "admin/home#index"
