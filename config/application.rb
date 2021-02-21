720ce5823a2fd12c187d1ea8ad509bffb72b86d9
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-20 23:01:31 -0700

    rename entire application "SwapRedFlag" -> "Swapp"

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SwapRedFlag
+module Swapp
   class Application < Rails::Application
     config.time_zone = "Mountain Time (US & Canada)"
     # Initialize configuration defaults for originally generated Rails version.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "swap_red_flag_production"
+  # config.active_job.queue_name_prefix = "swapp_production"
 
   config.action_mailer.perform_caching = false
 
