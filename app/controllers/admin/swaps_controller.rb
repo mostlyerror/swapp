c58bd88c755004455f8e6212724c87c89557e699
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-24 10:10:47 -0700

    availabilities flush and fill on update

--- app/controllers/admin/swaps_controller.rb
+++ app/controllers/admin/swaps_controller.rb
@@ -11,14 +11,20 @@ class Admin::SwapsController < Admin::BaseController
 
   def update_room_supply
     supply_params = params.require(:room_supply).permit!
+
     Swap.transaction do
-    swap = Swap.find(params[:id])
-    supply_params.each do |(motel_id, rooms)|
-      swap.availabilities.create!(
-        motel_id: motel_id,
-        date: Date.current, # this should come from the page.. cause what if you open the page before midnight, then submit afterwards...?
-        rooms: rooms
-      )
+      swap = Swap.find(params[:id])
+      swap.availabilities.destroy_all
+
+      supply_params
+      .reject {|_, v| v.blank? }
+      .each do |(motel_id, rooms)|
+        swap.availabilities.create!(
+          motel_id: motel_id,
+          date: Date.current, # this should come from the page.. cause what if you open the page before midnight, then submit afterwards...?
+          rooms: rooms
+        )
+      end
     end
 
     return redirect_to admin_home_path
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_163318) do
+ActiveRecord::Schema.define(version: 2021_02_24_002542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2021_02_23_163318) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "lat", precision: 10, scale: 7
+    t.decimal "lng", precision: 10, scale: 7
   end
 
   create_table "swaps", force: :cascade do |t|
