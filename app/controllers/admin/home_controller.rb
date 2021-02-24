a2b46f123f32b1da4ed1a2bed19bd412af16c3e8
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-24 11:35:46 -0700

    styling voucher supply table

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomeController < Admin::BaseController
   def index
     @swap = Swap.current
-    @availability = RoomAvailability.by_motel(@swap)
+    @availability = RoomSupply.by_motel(@swap)
     @vouchers = Voucher.order(created_at: :desc).all
   end
 end
