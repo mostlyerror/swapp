720c9fc516158f49a3b150057ddaecef6c97c954
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-25 01:14:29 -0700

    reorganizing admin dash

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,8 +1,11 @@
 class Admin::HomeController < Admin::BaseController
   def index
     @swap = Swap.current
-    @motel_map = Motel.all.pluck(:id, :name).to_h
-    @supply = RoomSupply.by_motel(@swap)
     @vouchers = Voucher.order(created_at: :desc).all
+
+    if SwapIntake.can_issue_voucher_today?(@swap)
+      @supply = RoomSupply.by_motel(@swap)
+      @motel_map = Motel.all.pluck(:id, :name).to_h
+    end
   end
 end
