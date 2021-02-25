b1917ba8cb3358ea7b05dd1687db64a4918abbba
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-02-24 20:05:01 -0800

    remove api key

--- app/javascript/packs/mapbox.js
+++ app/javascript/packs/mapbox.js
@@ -1,7 +1,6 @@
 import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiYmpvaG5zb245IiwiYSI6ImNrbGlib3dxYTJnd3Qyb3F0MXhrZzdoZHIifQ.5Yn7lVUa3_gRq9PVJSQ8Ig";
+mapboxgl.accessToken = "";
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
