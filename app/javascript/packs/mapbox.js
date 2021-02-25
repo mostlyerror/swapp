75fc663f13554af8579966ab70dd8598c8fd68e0
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-25 10:23:30 -0700

    we got the map to render! huzzah!

--- app/javascript/packs/mapbox.js
+++ app/javascript/packs/mapbox.js
@@ -1,9 +1,19 @@
-import mapboxgl from "mapbox-gl";
+import mapboxgl from '!mapbox-gl';
 
-mapboxgl.accessToken = "";
+// look up each motel on gmaps
+// get latlng from url
+// update seeds
+// get data from motels model 
+// 	embed the coordinates in the #map domNode with a data attr
+// 	embed the API key in the same way
+// pull both pieces of data from the domNode
+// make sure you supply LNGlat not LATlng to mapbox
+mapboxgl.accessToken = "pk.eyJ1IjoiYmpvaG5zb245IiwiYSI6ImNrbGlib3dxYTJnd3Qyb3F0MXhrZzdoZHIifQ.5Yn7lVUa3_gRq9PVJSQ8Ig";
+const lnglat = new mapboxgl.LngLat(-104.8360657, 39.9590737);
+
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: lnglat,
   zoom: 9, // starting zoom
 });
