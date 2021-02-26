40e4813460a5d8c9eddc288696f180608e11b5ba
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-02-25 17:34:18 -0800

    adds banner for active swap

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,8 @@
 class LandingController < ApplicationController
   before_action :authenticate_user!
   def index
+    @motel_map = Motel.all.pluck(:id, :name).to_h
+    @swap = Swap.current
+    @supply = RoomSupply.by_motel(@swap).to_h
   end
 end
