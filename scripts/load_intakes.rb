46232c9dfbffe5a28f711d1279fa6547611cbf05
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-27 10:06:58 -0700

    building a date parser/debugger. can get through the entire 1.1k sheet now

--- scripts/load_intakes.rb
+++ scripts/load_intakes.rb
@@ -1,65 +1,25 @@
-def parse(row)
-  row = row.first
-  {
-    date: parse_date_string(row[:date])
-  }
-end
-
-# def parse_date_string_debug(date_string, examples)
-#   d1, d2 = date_string.split("-")
-#   [d1, d2].compact.map {|dt| Date.parse(dt) }
-# rescue Date::Error => e
-#   begin
-#     [d1, d2].compact.map {|dt| Date.strptime(dt, "%m/%d/%Y") }
-#   rescue Date::Error => e
-#     examples.add(date_string)
-#   end
-# rescue NoMethodError => e
-#   examples.add(date_string)
-# end
-# unhandled values
-#     [0] nil,
-#     [1] "VOIDED",
-#     [2] "Voided"
-
-def parse_date(date)
-  a = ->(dt) { 
-    d1, d2 = dt.split("-")
-    [d1, d2].compact.map {|d| Date.parse(dt) }
-  }
-
-  b = ->(dt) { 
-    d1, d2 = dt.split("-")
-    [d1, d2].compact.map do |d| 
-      Date.strptime(d, "%m/%d/%Y")
-    end
-  }
-
-  val = nil
-  [a, b].each do |func|
-    val = func.call(date)
-    break if val
-  rescue
-    next
-  end
-  val
-end
-
-def parse(col, val)
-  return {
-    :date => parse_date(val)
-  }[col]
-end
-
-# filename = Rails.root.join("tmp", "intakes-10-2021-02-26.csv")
 filename = Rails.root.join("tmp", "intakes.csv")
-examples = Set.new
-errors = Set.new
-SmarterCSV.process(filename, verbose: true) do |row|
-  ap parse(:date, row.first[:date])
-  # row.first.each do |col, val|
-  #   ap parse(col, val)
-  # end
+opts = {headers: true}
+line = 1
+
+CSV.foreach(filename, opts) do |row|
+  line += 1
+  date_str = row['Date']
+
+  d1, d2 = date_str.to_s.split("-")
+  d1, d2 = [d1, d2].compact.map do |d|
+    Date.parse(d, year: 2020)
+  rescue Date::Error
+    begin 
+      Date.strptime(d, "%m/%d/%Y")
+    rescue Date::Error
+      # 2nd format mm/dd/yyyy failed
+    end
+  end
+
+  puts "row['Date']: #{date_str} d1:#{d1.to_s.rjust(8)} d2:#{d2}"
+rescue
+  puts "line in sheet: #{line}"
+  ap date_str
+  raise
 end
-
-
