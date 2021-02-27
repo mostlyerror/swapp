8b97c4341713680746e06b13f18b303481f40f58
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-27 12:00:29 -0700

    store raw phone numbers in clients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_082408) do
+ActiveRecord::Schema.define(version: 2021_02_27_185958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_082408) do
     t.string "ethnicity"
     t.string "email"
     t.string "phone_number"
+    t.string "phone_number_raw"
   end
 
   create_table "incident_reports", force: :cascade do |t|
