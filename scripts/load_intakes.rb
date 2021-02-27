5eb5a4577c43e28069c815cc93d5ae28c6ff6f87
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-27 11:23:07 -0700

    encapsulate date parsing a bit

--- scripts/load_intakes.rb
+++ scripts/load_intakes.rb
@@ -2,21 +2,17 @@ filename = Rails.root.join("intakes.csv")
 opts = {headers: true}
 line = 1
 
-formats = [
-  "%m/%d",    # m_d_no_year_slash
-  "%m-%d",    # m_d_no_year_dash
-  "%m/%d/%Y", # m_d_yy_slash
-  "%m-%d-%Y", # m_d_yy_dash
-  "%d-%b",    # d_b_no_year_dash 
-  "%d/%b",    # d_b_no_year_slash
-]
-
-CSV.foreach(filename, opts) do |row|
-  line += 1
-  date_str = row['Date'].to_s
-  d1, d2 = date_str.to_s.split("-")
-
-  d1, d2 = [d1, d2].compact.map do |d|
+def parse_date(val)
+  formats = [
+    "%m/%d",    # m_d_no_year_slash
+    "%m-%d",    # m_d_no_year_dash
+    "%m/%d/%Y", # m_d_yy_slash
+    "%m-%d-%Y", # m_d_yy_dash
+    "%d-%b",    # d_b_no_year_dash 
+    "%d/%b",    # d_b_no_year_slash
+  ]
+  d1, d2 = val.split("-")
+  [d1, d2].compact.map do |d|
     date = nil
     formats.each do |fmt|
       date = Date.strptime(d, fmt)
@@ -28,8 +24,13 @@ CSV.foreach(filename, opts) do |row|
     end
     date
   end
+end
 
-  puts "date_str: #{date_str.ljust(16)} d1: #{d1.to_s.ljust(16)} d2: #{d2}"
+CSV.foreach(filename, opts) do |row|
+  line += 1
+  date_str = row['Date'].to_s
+  dates = parse_date(date_str).compact
+  puts "line: #{line.to_s.ljust(16)} date_str: #{date_str.ljust(16)} output: #{dates}"
 rescue
   puts "line in sheet: #{line}"
   ap date_str
