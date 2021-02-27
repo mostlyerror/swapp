67144034d946f2cbcf9a790b293fd62b063f006b
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-27 11:00:09 -0700

    parsing dates ...seems.. to.. work.

--- scripts/load_intakes.rb
+++ scripts/load_intakes.rb
@@ -1,23 +1,35 @@
-filename = Rails.root.join("tmp", "intakes.csv")
+filename = Rails.root.join("intakes.csv")
 opts = {headers: true}
 line = 1
 
+formats = [
+  "%m/%d",    # m_d_no_year_slash
+  "%m-%d",    # m_d_no_year_dash
+  "%m/%d/%Y", # m_d_yy_slash
+  "%m-%d-%Y", # m_d_yy_dash
+  "%d-%b",    # d_b_no_year_dash 
+  "%d/%b",    # d_b_no_year_slash
+]
+
 CSV.foreach(filename, opts) do |row|
   line += 1
-  date_str = row['Date']
-
+  date_str = row['Date'].to_s
   d1, d2 = date_str.to_s.split("-")
+
   d1, d2 = [d1, d2].compact.map do |d|
-    Date.parse(d, year: 2020)
-  rescue Date::Error
-    begin 
-      Date.strptime(d, "%m/%d/%Y")
+    date = nil
+    formats.each do |fmt|
+      date = Date.strptime(d, fmt)
+      year = date.month > 6 ? 2020 : 2021
+      date = Date.new(year, date.month, date.day)
+      break
     rescue Date::Error
-      # 2nd format mm/dd/yyyy failed
+      next
     end
+    date
   end
 
-  puts "row['Date']: #{date_str} d1:#{d1.to_s.rjust(8)} d2:#{d2}"
+  puts "date_str: #{date_str.ljust(16)} d1: #{d1.to_s.ljust(16)} d2: #{d2}"
 rescue
   puts "line in sheet: #{line}"
   ap date_str
