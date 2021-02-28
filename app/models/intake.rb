cde80491bb78c3f3dfdcdbc186b92a14ceb46244
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-27 22:57:28 -0700

    build a re-usable question/answer setup

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -11,6 +11,21 @@ class Intake < ApplicationRecord
     "Yes - Seasonal/Temporary"
   ]
 
+  SLEEP_LAST_NIGHT = OpenStruct.new(
+    key: :where_did_you_sleep_last_night,
+    question: "Where did you sleep last night?",
+    answers: [
+      "In own home",
+      "In a shelter",
+      "In a motel",
+      "Doubled Up",
+      "In a car/RV",
+      "On the streets or in a tent",
+      "In the hospital",
+      "Incarcerated",
+    ]
+  )
+
   private 
 
     def trim_survey_fields
