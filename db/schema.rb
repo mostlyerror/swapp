500b69e1b97f18477de51768221914db738ee752
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-27 20:56:18 -0700

    cleaning up races table..

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_005937) do
+ActiveRecord::Schema.define(version: 2021_02_28_035607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,10 +70,6 @@ ActiveRecord::Schema.define(version: 2021_02_28_005937) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "pet_friendly"
-  end
-
-  create_table "races", force: :cascade do |t|
-    t.string "name", null: false
   end
 
   create_table "swaps", force: :cascade do |t|
