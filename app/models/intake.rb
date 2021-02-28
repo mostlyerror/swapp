22e21db5e17c70ca762aa26c9e78767f9832bb03
Ben Poon <benjamintpoon@gmail.com>
2021-02-28 07:15:02 -0700

    form questions powered by intake constants, "makeover" with (#52)
    * form questions powered by intake constants, "makeover" with
    tailwindcss-forms
    
    * fix too small inputs in admin voucher supply
    
    * apply overflow scrolling to admin vouchers table
    
    * fix bug in parsing races for new empty client record
    
    * re-add soopers question
    
    * combine save into one
    
    * extend intake form system test

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -4,8 +4,74 @@ class Intake < ApplicationRecord
   belongs_to :user
   before_save :trim_survey_fields
 
+  FIRST_NAME = OpenStruct.new(
+    key: 'first_name',
+    text: "First Name" 
+  )
+
+  LAST_NAME = OpenStruct.new(
+    key: 'last_name',
+    text: "Last Name"
+  )
+
+  DATE_OF_BIRTH = OpenStruct.new(
+    key: 'date_of_birth',
+    text: "Date of Birth"
+  )
+
+  GENDER = OpenStruct.new(
+    key: 'gender',
+    text: "Gender",
+    choices: Client::GENDER
+  )
+
+  RACE = OpenStruct.new(
+    key: 'race',
+    text: "Race",
+    choices: Client::RACE
+  )
+
+  ETHNICITY = OpenStruct.new(
+    key: 'ethnicity',
+    text: "Hispanic or Latino?",
+    choices: Client::ETHNICITY
+  )
+
+  HOMELESSNESS_FIRST_TIME = OpenStruct.new(
+    key: 'homelessness_first_time',
+    text: "Is this the first time you have been homeless?", 
+    choices: %w[ Yes No ]
+  )
+
+  HOMELESSNESS_HOW_LONG_THIS_TIME = OpenStruct.new(
+    key: 'homelessness_how_long_this_time',
+    text: "How long have you been homeless this time?", 
+    placeholder: "(e.g, 2 months, 5 years, etc.)"
+  )
+
+  HOMELESSNESS_EPISODES_LAST_THREE_YEARS = OpenStruct.new(
+    key: 'homelessness_episodes_last_three_years',
+    text: "Including this time, how many separate times have you stayed in shelters or on the streets in the past 3 years?", 
+    choices: [
+      "Fewer than 4 times",
+      "4 or more times"
+    ]
+  )
+
+  HOMELESSNESS_EPISODES_HOW_LONG = OpenStruct.new(
+    key: 'homelessness_episodes_how_long',
+    text: "In total, how long did you stay in shelters or on the streets those times?", 
+    placeholder: "(e.g, 2 months, 5 years, etc.)"
+  )
+
+  LIVING_IN_THIS_COMMUNITY = OpenStruct.new(
+    key: 'how_long_living_in_this_community',
+    text: "How long have you been living in this community?",
+    placeholder: "(e.g, 2 months, 5 years, etc.)"
+  )
+
   SLEEP_LAST_NIGHT = OpenStruct.new(
-    key: :where_did_you_sleep_last_night,
+    key: 'where_did_you_sleep_last_night',
     text: "Where did you sleep last night?",
     choices: [
       "In own home",
@@ -19,7 +85,7 @@ class Intake < ApplicationRecord
     ]
   )
   WHY_NOT_SHELTER = OpenStruct.new(
-    key: :why_not_shelter,
+    key: 'why_not_shelter',
     text: "What is the reason you have not accessed shelter?",
     choices: [
       "No room",
@@ -35,7 +101,7 @@ class Intake < ApplicationRecord
   )
 
   ARE_YOU_WORKING = OpenStruct.new(
-    key: :are_you_working,
+    key: 'are_you_working',
     text: "Are you working?",
     choices: [
       "No",
@@ -46,93 +112,154 @@ class Intake < ApplicationRecord
   )
 
   VETERAN = OpenStruct.new(
-    key: :armed_forces,
+    key: 'armed_forces',
     text: "Have you ever served in the US Armed Forces (Army, Navy, Air Force, Marines or Coast Guard)?", 
     choices: %w[ Yes No ]
   )
 
   ACTIVE_DUTY = OpenStruct.new(
-    key: :active_duty,
+    key: 'active_duty',
     text: "Were you ever called into active duty as a member of the National Guard or as a Reservist?",
     choices: %w[ Yes No ]
   )
 
   SUBSTANCE_ABUSE = OpenStruct.new(
-    key: :substance_abuse,
+    key: 'substance_abuse',
     text: "Do you have any Substance Abuse Issues?",
     choices: %w[ Yes No ]
   )
 
   SUBSTANCE_ABUSE_IMPAIRMENT = OpenStruct.new(
-    key: :substance_abuse_impairment,
+    key: 'substance_abuse_impairment',
     text: "If yes, is this a long-term disease that impairs your ability to hold a job or live independently?",
     choices: %w[ Yes No ]
   )
 
   CHRONIC_HEALTH_CONDITION = OpenStruct.new(
-    key: :chronic_health_condition,
+    key: 'chronic_health_condition',
     text: "Do you have a Chronic Health Condition?",
     choices: %w[ Yes No ]
   )
 
   CHRONIC_HEALTH_CONDITION_IMPAIRMENT = OpenStruct.new(
-    key: :chronic_health_condition_impairment,
+    key: 'chronic_health_condition_impairment',
     text: "If yes, is this a long-term disease that impairs your ability to hold a job or live independently?",
     choices: %w[ Yes No ]
   )
 
   MENTAL_HEALTH_CONDITION = OpenStruct.new(
-    key: :mental_health_condition,
+    key: 'mental_health_condition',
     text: "Do you have a Mental Health Condition?",
     choices: %w[ Yes No ]
   )
 
   MENTAL_HEALTH_CONDITION_IMPAIRMENT = OpenStruct.new(
-    key: :mental_health_condition_impairment,
+    key: 'mental_health_condition_impairment',
     text: "If yes, is this a long-term disease that impairs your ability to hold a job or live independently?",
     choices: %w[ Yes No ]
   )
 
   MENTAL_HEALTH_DISABILITY = OpenStruct.new(
-    key: :mental_health_disability,
+    key: 'mental_health_disability',
     text: "Do you have a Mental Health Disability?",
     choices: %w[ Yes No ]
   )
 
   MENTAL_HEALTH_DISABILITY_IMPAIRMENT = OpenStruct.new(
-    key: :mental_health_disability_impairment,
+    key: 'mental_health_disability_impairment',
     text: "If yes, is this a long-term disease that impairs your ability to hold a job or live independently?",
     choices: %w[ Yes No ]
   )
 
   PHYSICAL_DISABILITY = OpenStruct.new(
-    key: :physical_disability,
+    key: 'physical_disability',
     text: "Do you have a Physical Disability?",
     choices: %w[ Yes No ]
   )
 
   PHYSICAL_DISABILITY_IMPAIRMENT = OpenStruct.new(
-    key: :physical_disability_impairment,
+    key: 'physical_disability_impairment',
     text: "If yes, is this a long-term disease that impairs your ability to hold a job or live independently?",
     choices: %w[ Yes No ]
   )
 
   DEVELOPMENTAL_DISABILITY = OpenStruct.new(
-    key: :developmental_disability,
+    key: 'developmental_disability',
     text: "Do you have a Developmental Disability?",
     choices: %w[ Yes No ]
   )
 
   DEVELOPMENTAL_DISABILITY_IMPAIRMENT = OpenStruct.new(
-    key: :developmental_disability_impairment,
+    key: 'developmental_disability_impairment',
     text: "If yes, is this a long-term disease that impairs your ability to hold a job or live independently?",
     choices: %w[ Yes No ]
   )
 
   FLEEING_DOMESTIC_VIOLENCE = OpenStruct.new(
-    key: :fleeing_domestic_violence,
+    key: 'fleeing_domestic_violence',
     text: "Are you experiencing homelessness because you are fleeing Domestic Violence, Sexual Assault or Stalking?",
     choices: %w[ Yes No ]
+  )
+
+  NUM_ADULTS_HOUSEHOLD = OpenStruct.new(
+    key: 'num_adults_household',
+    text: "Number of adults in household",
+    placeholder: "enter numeric value"
+  )
+
+  NUM_CHILDREN_HOUSEHOLD = OpenStruct.new(
+    key: 'num_children_household',
+    text: "Number of children in household",
+    placeholder: "enter numeric value"
+  )
+
+  LAST_PERMANENT_RESIDENCE_CITY_AND_STATE = OpenStruct.new(
+    key: 'last_permanent_residence_city_and_state',
+    text: "City and State of Last Permanent Residence",
+    placeholder: "Brighton, CO"
+  )
+
+  LAST_PERMANENT_RESIDENCE_COUNTY= OpenStruct.new(
+    key: 'last_permanent_residence_county',
+    text: "County of Last Permanent Residence",
+    placeholder: "Adams"
+  )
+
+  KING_SOOPERS_CARD = OpenStruct.new(
+    key: 'king_soopers_card',
+    text: "King Soopers Card?",
+    choices: %w[ Yes No ]
+  )
+
+  BUS_PASS = OpenStruct.new(
+    key: 'bus_pass',
+    text: "Bus Pass?",
+    choices: %w[ Yes No ]
+  )
+
+  CHECK_IN = OpenStruct.new(
+    key: 'check_in',
+    text: "Check In"
+  )
+
+  CHECK_OUT = OpenStruct.new(
+    key: 'check_out',
+    text: "Check Out"
+  )
+
+  MOTEL_ID = OpenStruct.new(
+    key: 'motel_id',
+    text: "Hotel (vouchers remaining)", 
+  )
+
+  PHONE_NUMBER = OpenStruct.new(
+    key: 'phone_number',
+    text: "Phone Number (optional)"
+  )
+
+  EMAIL = OpenStruct.new(
+    key: 'email',
+    text: "Email (optional)"
   )
 
   private 
