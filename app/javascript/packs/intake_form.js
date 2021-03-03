72af31fe121f4747c3c80eed417b3464d72e2b0a
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-03 12:11:17 -0700

    add an "Other" option to "why have you not accessed shelter" question with write-in for different reasons

--- app/javascript/packs/intake_form.js
+++ app/javascript/packs/intake_form.js
@@ -1,11 +1,24 @@
-const input = document.getElementById(
+const sleepLastNightInput = document.getElementById(
   "intake_survey[where_did_you_sleep_last_night]_other"
 );
-const radio = document.getElementById(
+const sleepLastNightRadio = document.getElementById(
   "intake_survey[where_did_you_sleep_last_night]_other_radio"
 );
 
-input.onchange = function () {
-  radio.setAttribute("checked", "checked");
-  radio.value = `Other: ${input.value}`;
+sleepLastNightInput.onchange = function () {
+  sleepLastNightRadio.setAttribute("checked", "checked");
+  sleepLastNightRadio.value = `Other: ${sleepLastNightInput.value}`;
+};
+
+
+const whyNotShelterInput = document.getElementById(
+	"intake_survey[why_not_shelter]_other_text_field"
+);
+const whyNotShelterCheckbox = document.getElementById(
+	"intake_survey[why_not_shelter]_other_checkbox"
+);
+
+whyNotShelterInput.onchange = function () {
+  whyNotShelterCheckbox.setAttribute("checked", "checked");
+  whyNotShelterCheckbox.value = `Other: ${whyNotShelterInput.value}`;
 };
