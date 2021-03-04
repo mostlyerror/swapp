8c2cfb6642bff020be374bf27655cf89010a1922
Ben Poon <benjamintpoon@gmail.com>
2021-03-04 15:54:58 -0700

    Update landing_controller.rb
    this is all you need! everything else happens in the parent

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,4 @@
 class LandingController < ApplicationController
   def index
-    @swap = Swap.current
-    if @swap
-      @motel_map = Motel.all.pluck(:id, :name).to_h
-      @supply = RoomSupply.by_motel(@swap).to_h
-    end
   end
 end
