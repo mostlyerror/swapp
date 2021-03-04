12cebddea83af4d7d8c859bfd21951bcc2f19657
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-03 21:52:03 -0700

    cloning family member template nodes and appending to family list

--- app/javascript/packs/intake_form.js
+++ app/javascript/packs/intake_form.js
@@ -5,7 +5,7 @@ const sleepLastNightRadio = document.getElementById(
   "intake_survey[where_did_you_sleep_last_night]_other_radio"
 );
 
-sleepLastNightInput.onchange = function () {
+sleepLastNightInput.onchange = () => {
   sleepLastNightRadio.setAttribute("checked", "checked");
   sleepLastNightRadio.value = `Other: ${sleepLastNightInput.value}`;
 };
@@ -18,7 +18,24 @@ const whyNotShelterCheckbox = document.getElementById(
 	"intake_survey[why_not_shelter]_other_checkbox"
 );
 
-whyNotShelterInput.onchange = function () {
+whyNotShelterInput.onchange = () => {
   whyNotShelterCheckbox.setAttribute("checked", "checked");
   whyNotShelterCheckbox.value = `Other: ${whyNotShelterInput.value}`;
 };
+
+
+const addFamilyMemberBtn = document.getElementById("add_family_member");
+let familyMembers = 0;
+
+addFamilyMemberBtn.onclick = () => {
+	let template = document.getElementById("template_family_member");
+	let container = document.getElementById("family_members");
+	let clone = template.cloneNode(true)
+	clone.classList.remove('hidden')
+	clone.classList.add('block')
+	clone.id = `family_member_${++familyMembers}`
+	clone.querySelectorAll('input')
+	     .forEach(input => { input.value = '' });
+
+	container.appendChild(clone);
+}
