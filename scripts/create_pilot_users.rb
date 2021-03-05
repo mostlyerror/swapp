d07ea9a62ec27b4ec7b64fb32f10b2e0056ef4aa
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-03-04 17:52:52 -0800

    initial commit

--- scripts/create_pilot_users.rb
+++ scripts/create_pilot_users.rb
@@ -1,9 +1,9 @@
 # one time script to create all the identified pilot user accounts
 
-passwords = {
-  password: 'password',
-  password_confirmation: 'password'
-}
+# passwords = {
+  # password: 'password',
+  # password_confirmation: 'password'
+# }
 
 keys = [
   :email, :first_name, :last_name, :admin
@@ -18,19 +18,28 @@ user_data = [
   ['mcercone@adcogov.org', 'Max', 'Cercone', true],
   ['cjurischk@adcogov.org', 'Courtney', 'Jurischk', true],
   ['mrivera@adcogov.org', 'Matt', 'Rivera', true],
-  ['hmcclure@adcogov.org', 'Heather', 'McClure', true],
+  ['hmcclure@adcogov.org', 'Heather', 'McClure', false],
   ['jschultz@adcogov.org', 'Jason', 'Schultz', true],
   ['ashley@almosthomeonline.org', 'Ashley', 'Dunn', true],
   ['stephanie@almosthomeonline.org', 'Stephanie', 'Beazley', true],
   ['jeanette@almosthomeonline.org', 'Jeanette', 'Causey', true],
   ['nubia@almosthomeonline.org', 'Nubia', 'Saenz', true],
-  ['mayra.galaviz@thorntonco.gov', 'Mayra', 'Galaviz', true],
-  ['jaylin.stotler@thorntonco.gov', 'Jaylin', 'Stotler', true],
-  ['mario.solis-armenta@thorntonco.gov', 'Mario', 'Solis-Armenta', true],
-  ['rvenkatesh@northglenn.org', 'Rupa', 'Venkatesh', true],
-  ['jhulse@northglenn.org', 'Jessica', 'Hulse', true],
+  ['mayra.galaviz@thorntonco.gov', 'Mayra', 'Galaviz', false],
+  ['jaylin.stotler@thorntonco.gov', 'Jaylin', 'Stotler', false],
+  ['mario.solis-armenta@thorntonco.gov', 'Mario', 'Solis-Armenta', false],
+  ['rvenkatesh@northglenn.org', 'Rupa', 'Venkatesh', false],
+  ['jhulse@northglenn.org', 'Jessica', 'Hulse', false],
 ]
 
 user_data.each do |data|
+  first_name_part = data[1].slice(0, 3)
+  last_name_part = data[2].slice(0, 4)
+
+  passwords = {
+    password: '"#{first_name_part + last_name_part}2021".downcase',
+    password_confirmation:'"#{first_name_part + last_name_part}2021".downcase'
+
+}
+
   User.create! keys.zip(data).to_h.merge(passwords)
 end
