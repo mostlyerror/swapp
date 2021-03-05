cb2f06e3dc7afebc6abe574a459f10507ea676a1
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-05 01:00:36 -0700

    add family_members jsonb column to short intakes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_180713) do
+ActiveRecord::Schema.define(version: 2021_03_05_075712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_180713) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "client_id", null: false
     t.bigint "user_id", null: false
+    t.jsonb "family_members", default: {}
     t.index ["client_id"], name: "index_short_intakes_on_client_id"
     t.index ["user_id"], name: "index_short_intakes_on_user_id"
   end
