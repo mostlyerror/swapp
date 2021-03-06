267580706c4f5098ee03621038f53ee829b9dd51
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-06 11:53:39 -0700

    remove active flag from swaps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_091906) do
+ActiveRecord::Schema.define(version: 2021_03_06_181253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,7 +107,6 @@ ActiveRecord::Schema.define(version: 2021_03_05_091906) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "active", default: false
   end
 
   create_table "users", force: :cascade do |t|
