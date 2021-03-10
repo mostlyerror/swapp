1163d984adff9f95e1e27b49ba42c1eea7f34e6b
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-10 00:56:25 -0700

    relax not null constraint on incident_reporst.occurred_at since we don't have that data for any prior incident

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_181253) do
+ActiveRecord::Schema.define(version: 2021_03_10_075256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_181253) do
   create_table "incident_reports", force: :cascade do |t|
     t.bigint "client_id", null: false
     t.text "description"
-    t.datetime "occurred_at", null: false
+    t.datetime "occurred_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "reporter_id"
