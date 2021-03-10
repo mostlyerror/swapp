83f571ab3972458e069a3917d45796208c63b39d
Ben Poon <benjamintpoon@gmail.com>
2021-03-10 13:56:32 -0700

    swap activation state machine (#67)
    * add aasm gem
    
    * define swap state machine
    
    * add column swaps.aasm_state
    
    * failing test for swap activations & emails
    
    * test extending swap only extends voucher checkouts for that period
    
    * unhide extend button, fix text size bug for number of days input
    
    * move pilot user script into seeds
    
    * remove email assertions from swap activation test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_093933) do
+ActiveRecord::Schema.define(version: 2021_03_10_185054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_093933) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "aasm_state"
   end
 
   create_table "users", force: :cascade do |t|
