60d27e29910d64af2e77c8a649e2b6948c8f9a16
Ben Poon <benjamintpoon@gmail.com>
2021-03-11 10:40:33 -0700

    show voucher intake start/end and check-in/out on swap active notice (#70)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_185054) do
+ActiveRecord::Schema.define(version: 2021_03_11_173156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,8 @@ ActiveRecord::Schema.define(version: 2021_03_10_185054) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "aasm_state"
+    t.date "intake_start_date"
+    t.date "intake_end_date"
   end
 
   create_table "users", force: :cascade do |t|
