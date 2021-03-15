d09bd200c91f9df68ef3395ef93a68571b65a0c1
Ben Poon <benjamintpoon@gmail.com>
2021-03-14 21:52:36 -0600

    swap intake tests (#75)
    * correct error message key to match validation
    
    * intake start/end added to swaps factory definition
    
    * swap#intake_period based on db columns instead of derivation
    
    * simplify intake period test

--- test/models/intake_period_test.rb
+++ test/models/intake_period_test.rb
@@ -2,26 +2,18 @@ require 'test_helper'
 
 class SwapIntakeTest < ActiveSupport::TestCase
   setup do
-    @swap = build_stubbed(:swap, start_date: Date.current.tomorrow, end_date: Date.current.tomorrow  + 1)
+    @swap = build_stubbed(:swap, 
+                          start_date: Date.current.tomorrow, 
+                          end_date: Date.current.tomorrow + 1,
+                          intake_start_date: Date.current,
+                          intake_end_date: Date.current.tomorrow 
+                         )
   end
 
-  test "can issue vouchers 1 day before @swap period start" do
-    assert SwapIntake.can_issue_voucher_today?(@swap)
-  end
-
-  test "can issue vouchers 1 day before @swap period end" do
-    assert SwapIntake.can_issue_voucher_on?(@swap.end_date - 1.day, @swap)
-  end
-
-  test "can't issue vouchers before or after intake period" do
-    assert SwapIntake.can_issue_voucher_on?(@swap.start_date - 1.day, @swap)
-    refute SwapIntake.can_issue_voucher_on?(@swap.start_date - 2.days, @swap)
-
-    assert SwapIntake.can_issue_voucher_on?(@swap.end_date - 1.day, @swap)
-    refute SwapIntake.can_issue_voucher_on?(@swap.end_date + 1.days, @swap)
-  end
-
-  test "can't issue voucher on last day of @swap period" do
-    refute SwapIntake.can_issue_voucher_on?(@swap.end_date, @swap)
+  test "vouchers can only be issued during swap intake period" do
+    assert SwapIntake.can_issue_voucher_on?(Date.current, @swap)
+    assert SwapIntake.can_issue_voucher_on?(Date.current.tomorrow, @swap)
+    refute SwapIntake.can_issue_voucher_on?(Date.current - 1, @swap)
+    refute SwapIntake.can_issue_voucher_on?(Date.current.tomorrow + 1, @swap)
   end
 end
