e395bfd2997fd4121122a03271da6b4132def09f
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-15 17:40:53 -0600

    condensing event listeners into a loop

--- app/javascript/packs/intake_form.js
+++ app/javascript/packs/intake_form.js
@@ -1,447 +1,54 @@
-document.addEventListener("click", (event) => {
-  // HOMELESSNESS FIRST TIME
-
-  if (event.target.matches("#homelessness_first_time_yes")) {
-    const radio = document.getElementById(
-      "intake_homelessness_first_time_true"
-    );
-    const button = document.getElementById("homelessness_first_time_yes");
-    const radioNo = document.getElementById(
-      "intake_homelessness_first_time_false"
-    );
-    const buttonNo = document.getElementById("homelessness_first_time_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#homelessness_first_time_no")) {
-    const radio = document.getElementById(
-      "intake_homelessness_first_time_false"
-    );
-    const button = document.getElementById("homelessness_first_time_no");
-    const radioYes = document.getElementById(
-      "intake_homelessness_first_time_true"
-    );
-    const buttonYes = document.getElementById("homelessness_first_time_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // EPISODES LAST THREE YEARS
-
-  if (
-    event.target.matches(
-      "#episodes_last_three_years_fewer_than_four_times_fewer"
-    )
-  ) {
-    const radio = document.getElementById(
-      "intake_episodes_last_three_years_fewer_than_four_times_true"
-    );
-    const button = document.getElementById(
-      "episodes_last_three_years_fewer_than_four_times_fewer"
-    );
-    const radioMore = document.getElementById(
-      "intake_episodes_last_three_years_fewer_than_four_times_false"
-    );
-    const buttonMore = document.getElementById(
-      "episodes_last_three_years_fewer_than_four_times_more"
-    );
-
-    buttonMore.classList.remove("bg-indigo-900");
-    buttonMore.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioMore.value = "false";
-  }
-
-  if (
-    event.target.matches(
-      "#episodes_last_three_years_fewer_than_four_times_more"
-    )
-  ) {
-    const radio = document.getElementById(
-      "intake_episodes_last_three_years_fewer_than_four_times_false"
-    );
-    const button = document.getElementById(
-      "episodes_last_three_years_fewer_than_four_times_more"
-    );
-    const radioFewer = document.getElementById(
-      "intake_episodes_last_three_years_fewer_than_four_times_true"
-    );
-    const buttonFewer = document.getElementById(
-      "episodes_last_three_years_fewer_than_four_times_fewer"
-    );
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioFewer.value = "false";
-
-    buttonFewer.classList.remove("bg-indigo-900");
-    buttonFewer.classList.remove("text-white");
-  }
-
-  // ARMED FORCES
-
-  if (event.target.matches("#armed_forces_yes")) {
-    const radio = document.getElementById("intake_armed_forces_true");
-    const button = document.getElementById("armed_forces_yes");
-    const radioNo = document.getElementById("intake_armed_forces_false");
-    const buttonNo = document.getElementById("armed_forces_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#armed_forces_no")) {
-    const radio = document.getElementById("intake_armed_forces_false");
-    const button = document.getElementById("armed_forces_no");
-    const radioYes = document.getElementById("intake_armed_forces_true");
-    const buttonYes = document.getElementById("armed_forces_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // ACTIVE DUTY NATIONAL GUARD
-
-  if (event.target.matches("#active_duty_yes")) {
-    const radio = document.getElementById("intake_active_duty_true");
-    const button = document.getElementById("active_duty_yes");
-    const radioNo = document.getElementById("intake_active_duty_false");
-    const buttonNo = document.getElementById("active_duty_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#active_duty_no")) {
-    const radio = document.getElementById("intake_active_duty_false");
-    const button = document.getElementById("active_duty_no");
-    const radioYes = document.getElementById("intake_active_duty_true");
-    const buttonYes = document.getElementById("active_duty_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // SUBSTANCE ABUSE
-
-  if (event.target.matches("#substance_abuse_yes")) {
-    const radio = document.getElementById("intake_substance_abuse_true");
-    const button = document.getElementById("substance_abuse_yes");
-    const radioNo = document.getElementById("intake_substance_abuse_false");
-    const buttonNo = document.getElementById("substance_abuse_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#substance_abuse_no")) {
-    const radio = document.getElementById("intake_substance_abuse_false");
-    const button = document.getElementById("substance_abuse_no");
-    const radioYes = document.getElementById("intake_substance_abuse_true");
-    const buttonYes = document.getElementById("substance_abuse_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // CHRONIC HEALTH CONDITION
-
-  if (event.target.matches("#chronic_health_condition_yes")) {
-    const radio = document.getElementById(
-      "intake_chronic_health_condition_true"
-    );
-    const button = document.getElementById("chronic_health_condition_yes");
-    const radioNo = document.getElementById(
-      "intake_chronic_health_condition_false"
-    );
-    const buttonNo = document.getElementById("chronic_health_condition_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#chronic_health_condition_no")) {
-    const radio = document.getElementById(
-      "intake_chronic_health_condtion_false"
-    );
-    const button = document.getElementById("chronic_health_condition_no");
-    const radioYes = document.getElementById(
-      "intake_chronic_health_condition_true"
-    );
-    const buttonYes = document.getElementById("chronic_health_condition_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // MENATL HEALTH CONDITION
-
-  if (event.target.matches("#mental_health_condition_yes")) {
-    const radio = document.getElementById(
-      "intake_mental_health_condition_true"
-    );
-    const button = document.getElementById("mental_health_condition_yes");
-    const radioNo = document.getElementById(
-      "intake_mental_health_condition_false"
-    );
-    const buttonNo = document.getElementById("mental_health_condition_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#mental_health_condition_no")) {
-    const radio = document.getElementById(
-      "intake_mental_health_condition_false"
-    );
-    const button = document.getElementById("mental_health_condition_no");
-    const radioYes = document.getElementById(
-      "intake_mental_health_condition_true"
-    );
-    const buttonYes = document.getElementById("mental_health_condition_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // MENTAL HEALTH DISABILITY
-
-  if (event.target.matches("#mental_health_disability_yes")) {
-    const radio = document.getElementById(
-      "intake_mental_health_disability_true"
-    );
-    const button = document.getElementById("mental_health_disability_yes");
-    const radioNo = document.getElementById(
-      "intake_mental_health_disability_false"
-    );
-    const buttonNo = document.getElementById("mental_health_disability_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#mental_health_disability_no")) {
-    const radio = document.getElementById(
-      "intake_mental_health_disability_false"
-    );
-    const button = document.getElementById("mental_health_disability_no");
-    const radioYes = document.getElementById(
-      "intake_mental_health_disability_true"
-    );
-    const buttonYes = document.getElementById("mental_health_disability_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // PHYSICAL DISABILITY
-
-  if (event.target.matches("#physical_disability_yes")) {
-    const radio = document.getElementById("intake_physical_disability_true");
-    const button = document.getElementById("physical_disability_yes");
-    const radioNo = document.getElementById("intake_physical_disability_false");
-    const buttonNo = document.getElementById("physical_disability_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#physical_disability_no")) {
-    const radio = document.getElementById("intake_physical_disability_false");
-    const button = document.getElementById("physical_disability_no");
-    const radioYes = document.getElementById("intake_physical_disability_true");
-    const buttonYes = document.getElementById("physical_disability_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // DEVELOPMENTAL DISABILITY
-
-  if (event.target.matches("#developmental_disability_yes")) {
-    const radio = document.getElementById(
-      "intake_developmental_disability_true"
-    );
-    const button = document.getElementById("developmental_disability_yes");
-    const radioNo = document.getElementById(
-      "intake_developmental_disability_false"
-    );
-    const buttonNo = document.getElementById("developmental_disability_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#developmental_disability_no")) {
-    const radio = document.getElementById(
-      "intake_developmental_disability_false"
-    );
-    const button = document.getElementById("developmental_disability_no");
-    const radioYes = document.getElementById(
-      "intake_developmental_disability_true"
-    );
-    const buttonYes = document.getElementById("developmental_disability_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-
-  // DOMESTIC VIOLENCE, SEXUAL ASSAULT AND STALKING
-
-  if (event.target.matches("#fleeing_domestic_violence_yes")) {
-    const radio = document.getElementById(
-      "intake_fleeing_domestic_violence_true"
-    );
-    const button = document.getElementById("fleeing_domestic_violence_yes");
-    const radioNo = document.getElementById(
-      "intake_fleeing_domestic_violence_false"
-    );
-    const buttonNo = document.getElementById("fleeing_domestic_violence_no");
-
-    buttonNo.classList.remove("bg-indigo-900");
-    buttonNo.classList.remove("text-white");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioNo.value = "false";
-  }
-
-  if (event.target.matches("#fleeing_domestic_violence_no")) {
-    const radio = document.getElementById(
-      "intake_fleeing_domestic_violence_false"
-    );
-    const button = document.getElementById("fleeing_domestic_violence_no");
-    const radioYes = document.getElementById(
-      "intake_fleeing_domestic_violence_true"
-    );
-    const buttonYes = document.getElementById("fleeing_domestic_violence_yes");
-
-    button.classList.add("bg-indigo-900");
-    button.classList.add("text-white");
-
-    radio.value = "true";
-    radioYes.value = "false";
-
-    buttonYes.classList.remove("bg-indigo-900");
-    buttonYes.classList.remove("text-white");
-  }
-});
+questionKeys = [
+	"intake_homelessness_first_time",
+	"intake_episodes_last_three_years_fewer_than_four_times",
+	"intake_armed_forces",
+	"intake_active_duty",
+	"intake_substance_abuse",
+	"intake_chronic_health_condition",
+	"intake_mental_health_condition",
+	"intake_mental_health_disability",
+	"intake_physical_disability",
+	"intake_developmental_disability",
+	"intake_fleeing_domestic_violence",
+].forEach(key => {
+	document.addEventListener('click', event => {
+		const radioTrue = document.getElementById(`${key}_true`);
+		const radioFalse = document.getElementById(`${key}_false`);
+	        const buttonYes = document.getElementById(`${key}_yes`);
+		const buttonNo = document.getElementById(`${key}_no`);
+
+		if (event.target.id === `${key}_yes`) {
+			buttonNo.classList.add("outline-gray-300");
+			buttonNo.classList.add("border-gray-300");
+			buttonNo.classList.add("text-gray-300");
+			buttonNo.classList.remove("bg-indigo-700");
+			buttonNo.classList.remove("text-white");
+
+			buttonYes.classList.add("bg-indigo-700");
+			buttonYes.classList.add("text-white");
+			buttonYes.classList.remove("outline-gray-300");
+			buttonYes.classList.remove("border-gray-300");
+			buttonYes.classList.remove("text-gray-300");
+
+			radioTrue.checked = true;
+			radioFalse.checked = false;
+		}
+
+		if (event.target.id === `${key}_no`) {
+			buttonYes.classList.add("outline-gray-300");
+			buttonYes.classList.add("outline-gray-300");
+			buttonYes.classList.add("text-gray-300");
+			buttonYes.classList.remove("bg-indigo-700");
+			buttonYes.classList.remove("text-white");
+
+			buttonNo.classList.add("bg-indigo-700");
+			buttonNo.classList.add("text-white");
+			buttonNo.classList.remove("outline-gray-300");
+			buttonNo.classList.remove("border--gray-300");
+			buttonNo.classList.remove("text-gray-300");
+
+			radioTrue.checked = false;
+			radioFalse.checked = true;
+		}
+	})
+})
