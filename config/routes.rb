2ef7210a34674a92c8fe6a07fd91191755d624d4
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-03-23 16:12:32 -0700

    working on detailed view

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "vouchers/:id/created" => "vouchers#created", as: :voucher_created
   resources :swaps
   get "/motels", to: "motels#index"
+  get "/motels/client/:id" => "motels#show", as: :client_voucher
 
   namespace :admin do
     get "/" => "home#index", as: :home
