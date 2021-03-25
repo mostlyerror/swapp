7c6d28bbff8b9d6b9dc62ba8ef4f5b0682bffd80
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-03-25 16:46:11 -0700

    adds a check on client controller for hotel_user

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,9 @@
 class ClientsController < ApplicationController
   def index
+    if current_user.hotel_user?
+      redirect_to hotels_home_path
+    end
+
     @q = Client.ransack(params[:q])
     @searched = !params[:q].nil?
     @searched_term = params[:q]&.values&.first
@@ -7,6 +11,10 @@ class ClientsController < ApplicationController
   end
 
   def show
+    if current_user.hotel_user?
+      redirect_to hotels_home_path
+    end
+
     @client = Client.find(params[:id])
     @existing_voucher = @swap&.vouchers&.find_by(client: @client)
   end
