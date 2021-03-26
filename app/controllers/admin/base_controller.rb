eb45677a560915deed89505cc6ac864fc19778f0
Benjamin Poon <benjamintpoon@gmail.com>
2021-03-25 18:54:21 -0600

    authentication and admin_user authorization logic not needed in admin base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,3 @@
 class Admin::BaseController < ApplicationController
   layout 'admin/admin'
-  before_action :authenticate_user!
-  before_action do 
-    if !current_user.admin_user?
-      redirect_to new_user_session_path
-    end
-  end
 end
