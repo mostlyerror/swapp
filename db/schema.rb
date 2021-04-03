ab1821cca7e9c3be24e971c780562013fcbcee64
Benjamin Poon <benjamintpoon@gmail.com>
2021-04-03 04:44:04 -0600

    setting up phonetic clients search

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_205846) do
+ActiveRecord::Schema.define(version: 2021_04_02_221229) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
 
   create_table "availabilities", force: :cascade do |t|
