b4acf635d4e7532d3213afb68399814eb090f0a5
Benjamin Poon <benjamintpoon@gmail.com>
2021-04-07 13:49:27 -0600

    add jQuery and easy-autocomplete libraries, update webpack config to provide globally

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-
 require("alpinejs");
 import "stylesheets/application.scss";
 
