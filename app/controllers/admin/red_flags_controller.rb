2aca18df918555412dfd894614a194b4a88f7820
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-04-19 14:07:39 -0700

    removes client variable

--- app/controllers/admin/red_flags_controller.rb
+++ app/controllers/admin/red_flags_controller.rb
@@ -10,12 +10,12 @@ class Admin::RedFlagsController < Admin::BaseController
             RedFlag.where(client_id: params[:id]).destroy_all
 
             red_flag_attrs = params[:client][:hotel_ids].map do |hotel_id|
-                {client_id: client.id, hotel_id: hotel_id}
+                {client_id: params[:id], hotel_id: hotel_id}
             end
 
 
             if RedFlag.create(red_flag_attrs)
-                redirect_to hotels_show_client_path(client), info: "Update successful!"
+                redirect_to hotels_show_client_path(params[:id]), info: "Update successful!"
             end
         end
     end
