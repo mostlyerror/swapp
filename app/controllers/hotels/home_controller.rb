d41de0cae1e4a37e95852c2290f1b0fb9b65718c
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-04-22 15:32:58 -0700

    places view branching into controller

--- app/controllers/hotels/home_controller.rb
+++ app/controllers/hotels/home_controller.rb
@@ -12,5 +12,9 @@ class Hotels::HomeController < Hotels::BaseController
     @flag_hotel_ids = @client.hotels.pluck(:id)
     @flagged_stamps = RedFlag.where(client: @client).pluck(:created_at)
     @incident_report = IncidentReport.new
+
+    @incident_reports = @client.incident_reports.order(created_at: :desc).select do |ir| 
+      current_user.admin_user? || current_user == ir.reporter
+    end
   end
 end
--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -104,7 +104,6 @@ class VouchersController < ApplicationController
   end
 
   def set_voucher_supply_for_hotel_dropdown
-    # flagged_hotels = Client.find(voucher_params[:client][:id]).hotels.id
     @client = Client.find(params[:client_id] || voucher_params[:client][:id])
     flagged_hotels = @client.hotels.ids
     if @swap
