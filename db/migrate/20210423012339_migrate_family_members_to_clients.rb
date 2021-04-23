dac64f227ec4e87d3166c7140af23d0fdb9c9bbe
Benjamin Poon <benjamintpoon@gmail.com>
2021-04-23 07:56:20 -0600

    remove data migration for family column (nobody's used it in prod)

--- db/migrate/20210423012339_migrate_family_members_to_clients.rb
+++ db/migrate/20210423012339_migrate_family_members_to_clients.rb
@@ -1,13 +1,11 @@
 class MigrateFamilyMembersToClients < ActiveRecord::Migration[6.0]
   def self.up
     add_column :clients, :family_members, :jsonb, default: {}, using: 'family_members::jsonb'
-    execute "UPDATE clients c SET family_members = (SELECT family_members FROM short_intakes si where si.client_id = c.id);"
     remove_column :short_intakes, :family_members, :jsonb
   end
 
   def self.down
     add_column :short_intakes , :family_members, :jsonb, default: {}, using: 'family_members::jsonb'
-    execute "UPDATE short_intakes si SET family_members = (SELECT family_members FROM clients c where si.client_id = c.id);"
     remove_column :clients, :family_members, :jsonb
   end
 end
