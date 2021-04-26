9bda11bbf83a20a7d166402b13e0ae8086414b0f
Benjamin Poon <benjamintpoon@gmail.com>
2021-04-26 13:18:50 -0600

    add chokidar watch for *.erb files

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,12 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const environment = require('./environment')
+const environment = require("./environment");
+const chokidar = require("chokidar");
 
-module.exports = environment.toWebpackConfig()
+environment.config.devServer.before = (app, server) => {
+  chokidar
+    .watch(["config/locales/*.yml", "app/views/**/*.erb"])
+    .on("change", () => server.sockWrite(server.sockets, "content-changed"));
+};
+
+module.exports = environment.toWebpackConfig();
