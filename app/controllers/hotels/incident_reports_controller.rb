5801b134b9f4589067c459279ec4bfb4c2a8e51f
Benjamin Poon <benjamintpoon@gmail.com>
2021-04-27 20:45:25 -0600

    new incident report form

--- app/controllers/hotels/incident_reports_controller.rb
+++ app/controllers/hotels/incident_reports_controller.rb
@@ -1,17 +1,18 @@
 class Hotels::IncidentReportsController < Hotels::BaseController
     def create
         @incident_report = IncidentReport.new(incident_report_params)
-    
-        @incident_report.client_id = params[:id]
         @incident_report.reporter_id = current_user.id
-    
-        if @incident_report.save
-          redirect_to hotels_show_client_path(id: params[:id])
+
+        if !@incident_report.save
+          puts 'poop'
+          # handle some errors tho...
         end
+
+        redirect_to hotels_vouchers_path(params[:voucher_id])
       end
 
   private
   def incident_report_params
-    params.require(:incident_report).permit(:occurred_at, :description)
+    params.require(:incident_report).permit(:occurred_at, :description, :client_id)
   end
 end
--- app/controllers/hotels/vouchers_controller.rb
+++ app/controllers/hotels/vouchers_controller.rb
@@ -2,5 +2,6 @@ class Hotels::VouchersController < Hotels::BaseController
   def show
     @voucher = Voucher.find(params[:id])
     @incidents = @voucher.client.incident_reports
+    @incident_report = IncidentReport.new
   end
 end
