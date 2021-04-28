4ad7619e703b10610fc5922f52b446be38a305ca
Benjamin Poon <benjamintpoon@gmail.com>
2021-04-28 17:23:03 -0600

    add RFL update form to voucher page

--- app/controllers/admin/red_flags_controller.rb
+++ app/controllers/admin/red_flags_controller.rb
@@ -1,20 +1,20 @@
 class Admin::RedFlagsController < Admin::BaseController
-    add_flash_types :info, :error
+  add_flash_types :info, :error
 
-    def edit_red_flag
-        RedFlag.transaction do 
-            RedFlag.where(client_id: params[:id]).destroy_all
+  def edit_red_flag
+    client_id = params[:id]
+    RedFlag.transaction do 
+      RedFlag.where(client_id: client_id).destroy_all
 
-            red_flag_attrs = params[:client][:hotel_ids].map do |hotel_id|
-                {client_id: params[:id], hotel_id: hotel_id}
-            end
+      red_flag_attrs = params[:client][:hotel_ids].map do |hotel_id|
+        {client_id: client_id, hotel_id: hotel_id}
+      end
 
+      if RedFlag.create(red_flag_attrs)
+        return redirect_to hotels_show_client_path(client_id), info: "Update successful!"
+      end
 
-            if RedFlag.create(red_flag_attrs)
-                redirect_to hotels_show_client_path(params[:id]), info: "Update successful!"
-            else
-                redirect_to hotels_show_client_path(params[:id]), error: "An error has occurred. Please try again."
-            end
-        end
+      redirect_to hotels_show_client_path(client_id), error: "An error has occurred. Please try again."
     end
+  end
 end
--- app/controllers/hotels/vouchers_controller.rb
+++ app/controllers/hotels/vouchers_controller.rb
@@ -3,5 +3,10 @@ class Hotels::VouchersController < Hotels::BaseController
     @voucher = Voucher.find(params[:id])
     @incidents = @voucher.client.incident_reports
     @incident_report = IncidentReport.new
+
+    @client = @voucher.client
+    @hotels = Hotel.all
+    @flag_hotel_ids = @client.hotels.pluck(:id)
+    @flagged_stamps = RedFlag.where(client: @client).pluck(:created_at)
   end
 end
