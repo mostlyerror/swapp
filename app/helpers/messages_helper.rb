51c2dab329fd6b159e502b5ca2cf690922dbb185
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-04-30 12:00:20 -0700

    returns correct format in sms

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,27 +1,28 @@
 module MessagesHelper
 
     def parse_sms(sms)
-         
-        body = sms[:Body]&.strip&.upcase
-        from = sms[:From]
+        if Swap.current
+            body = sms[:Body]&.strip&.upcase
+            from = sms[:From]
+            
+            case body
 
-        case body
+            when "SWAP"
+                hotels = Hotel.all.pluck(:name)
+                vouchers_today = RoomSupply.vouchers_remaining_today(Swap.current)
+                vacancies = vouchers_today.map do |(hotel_id, vouchers)|
+                    "#{hotels[hotel_id]} - #{vouchers}\n"
+                end
 
-        when "SWAP"
-            hotels = Hotel.all.pluck(:name)
-            vouchers_today = RoomSupply.vouchers_remaining_today(Swap.current)
-            vacancies = vouchers_today.map do |(hotel_id, vouchers)|
-                # {name: hotels[hotel_id], vouchers: vouchers }]
-                puts "HOTEL: #{hotels[hotel_id]}, VOUCHERS: #{vouchers}"
+                return "Today's Availability (#{Date.current}):\n\n#{vacancies.join()}" 
+
+            when "ACTIVATE"
+                return "You messaged #{body}"
+            else
+                return "Unknown command. The available commands are SWAP and ACTIVATE. Please try again."
             end
-
-
-            return "#{vacancies}"
-        
-        when "ACTIVATE"
-            return "You messaged #{body}"
         else
-            return "Unknown command. The available commands are SWAP and ACTIVATE. Please try again."
+            return "Swap is not currently active. Please check your activation email for details."
+        end
     end
-end
 end
