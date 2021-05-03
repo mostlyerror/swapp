73ac6805fd4b2f362887450523fa5171f44f991e
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-05-03 11:34:07 -0700

    fixes sms bug

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -8,18 +8,16 @@ module MessagesHelper
             case body
 
             when "SWAP"
-                hotels = Hotel.all.pluck(:name)
+                hotels = Hotel.all.pluck(:id, :name).to_h
                 vouchers_today = RoomSupply.vouchers_remaining_today(Swap.current)
-                vacancies = vouchers_today.map do |(hotel_id, vouchers)|
+                vacancies = vouchers_today.map do |hotel_id, vouchers|
                     "#{hotels[hotel_id]} - #{vouchers}\n"
                 end
-
+                
                 return "Today's Availability (#{Date.current}):\n\n#{vacancies.join()}" 
 
-            when "ACTIVATE"
-                return "You messaged #{body}"
             else
-                return "Unknown command. The available commands are SWAP and ACTIVATE. Please try again."
+                return "Thank you for contacting the Swap hotline. The available command is SWAP. Please try again."
             end
         else
             return "Swap is not currently active. Please check your activation email for details."
