6e0ba0dff5f865a140944e7b2dd05548bba3ab7e
Benjamin Poon <benjamintpoon@gmail.com>
2021-05-10 12:33:29 -0600

    family members: switch cancel behavior for remove

--- app/javascript/components/FamilyMemberForm.js
+++ app/javascript/components/FamilyMemberForm.js
@@ -24,8 +24,30 @@ class FamilyMemberForm extends Component {
           />
         </div>
         <div>
-          <button type="button" className="" onClick={this.props.cancel}>
-            Cancel
+          <label htmlFor="relationship">Relationship</label>
+          <input
+            type="text"
+            name="relationship"
+            onChange={this.props.handleChange}
+            value={this.props.familyMember.relationship}
+          />
+        </div>
+        <div>
+          <label htmlFor="dateOfBirth">Date of Birth</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            onChange={this.props.handleChange}
+            value={this.props.familyMember.dateOfBirth}
+          />
+        </div>
+        <div>
+          <button
+            type="button"
+            className=""
+            onClick={(event) => this.props.remove(this.props.familyMember.id)}
+          >
+            Remove
           </button>
           <button type="button" className="" onClick={this.props.save}>
             Save
