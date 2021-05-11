9df24e6662d7115977321c29f51e25762f99c84b
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-05-11 16:54:01 -0700

    fix environ

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => Rails.application.credentials[:username],# This is the string literal 'apikey', NOT the ID of your API key
+    :user_name => Rails.application.credentials.sendgrid[:username],# This is the string literal 'apikey', NOT the ID of your API key
     :password => Rails.application.credentials.sendgrid[:password], # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
