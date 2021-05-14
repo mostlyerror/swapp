e305d753526f3d1db86e1c80fea62bdbed7d446c
Benjamin Poon <benjamintpoon@gmail.com>
2021-05-14 12:54:45 -0600

    tabletize client page

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -5,7 +5,7 @@ module ClientsHelper
 
   def button_class(disabled)
     disabled ?
-      "bg-gray-100 hover:none text-gray-500 text-center px-4 py-1 rounded-lg shadow cursor-not-allowed" :
-      "bg-indigo-600 hover:bg-indigo-700 text-white text-center px-4 py-1 rounded-lg shadow"
+      "bg-gray-100 hover:none text-gray-500 text-center px-4 md:px-8 py-1 md:py-2 rounded-lg shadow cursor-not-allowed" :
+      "bg-indigo-600 hover:bg-indigo-700 text-white text-center px-4 md:px-8 py-1 md:py-2 rounded-lg shadow"
   end
 end
