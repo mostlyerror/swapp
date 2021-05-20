6c3f7212e40e6657b4edae241322e2a95540ca3b
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-05-20 13:11:19 -0700

    vouchers can be sent by text

--- app/models/voucher_texter.rb
+++ app/models/voucher_texter.rb
@@ -1,21 +1,19 @@
 class VoucherTexter
   def initialize(voucher, sid, token, phone_number)
-    @voucher = voucher
-    @twilio_number = twilio_number
-    @client = Twilio::REST::Client.new(sid, token)
+    @voucher = Voucher.find(params[:id])
+    @twilio_number = phone_number
   end
 
-  def send_sms
+  def self.send_sms(voucher, sid, token)
+    @voucher = Voucher.find(voucher.id)
+    @client = Twilio::REST::Client.new(sid, token)
+    body = "Thanks, #{@voucher.client.name}! Your voucher for #{@voucher.hotel.name} is confirmed!\n\nVoucher #: #{@voucher.number}\n\n#{@voucher.hotel.name} is expecting you from #{@voucher.check_in} to #{@voucher.check_out}.\n\nAddress:\n\n#{@voucher.hotel.street_address}\n#{@voucher.hotel.address_second}\n#{@voucher.hotel.phone}"
+
     @client.messages.create(
-      from: @twilio_number,
+      from: ENV["TWILIO_NUMBER"],
       to: @voucher.client.phone_number,
       body: body
     )
   end
-  
-  private
 
-  def body
-    "Thanks, #{@voucher.client.name}! Your voucher for #{@voucher.hotel.name} is confirmed!\n\nVoucher #: #{@voucher.number}\n\n#{@voucher.hotel.name} is expecting you from #{@voucher.check_in} to #{@voucher.check_out}.\n\nAddress:\n\n#{@voucher.hotel.street_address}\n#{@voucher.hotel.address_second}\n#{@voucher.hotel.phone}"
-  end
 end
