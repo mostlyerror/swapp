0b932bb2630bb06aba0a557759d084d7fdb48ed1
Benjamin Poon <benjamintpoon@gmail.com>
2021-05-21 16:48:11 -0600

    add autoprefixer to webpack pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("tailwindcss")("./app/javascript/stylesheets/tailwind.config.js"),
+    require("autoprefixer"),
     require("postcss-import"),
     require("postcss-flexbugs-fixes"),
     require("postcss-preset-env")({
@@ -10,4 +11,4 @@ module.exports = {
       stage: 3,
     }),
   ],
-}
+};
