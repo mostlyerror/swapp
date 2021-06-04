ec68cb596970726972ba3ff6198c31643c8f8fcf
Benjamin Poon <benjamintpoon@gmail.com>
2021-06-04 17:04:51 -0600

    ButtonOutline component inherits background-color

--- app/javascript/components/SwapWizard/ButtonOutline.js
+++ app/javascript/components/SwapWizard/ButtonOutline.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ButtonOutline = (props) => {
   return (
     <button
-      className="px-4 py-2 rounded-md bg-white border border-gray-700 hover:bg-grey-50 text-gray-900 text-base"
+      className="px-4 py-2 rounded-md border border-gray-700 hover:bg-grey-50 text-gray-900"
       type="button"
       onClick={props.onClick}
       {...props}
