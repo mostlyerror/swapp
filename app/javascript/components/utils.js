e7b42f0b5a5cc041b4c82ec0e68b5e3396344b5a
Benjamin Poon <benjamintpoon@gmail.com>
2021-06-05 09:16:46 -0600

    util function to generate range of days

--- app/javascript/components/utils.js
+++ app/javascript/components/utils.js
@@ -1,5 +1,16 @@
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ')
 }
 
-export { classNames };
+function getDaysArray(start, end) {
+  for (
+    var arr = [], dt = new Date(start);
+    dt <= end;
+    dt.setDate(dt.getDate() + 1)
+  ) {
+    arr.push(new Date(dt))
+  }
+  return arr
+}
+
+export { classNames, getDaysArray }
