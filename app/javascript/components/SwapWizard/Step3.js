10e6fe0284ceb16c9ede4a737e222d8935ce72dd
Ben Poon <benjamintpoon@gmail.com>
2021-06-09 19:46:59 -0600

    Awesome admin past stay dates disabled (#120)
    * stay date picker: disable past day clicks & visually
    
    * cleanup typo

--- app/javascript/components/SwapWizard/Step3.js
+++ app/javascript/components/SwapWizard/Step3.js
@@ -12,12 +12,12 @@ export const Step3 = (props) => {
         <h3 className="font-semibold">Set Intake Days</h3>
         <p className="mt-4 text-xl">
           On each <span className="font-semibold italic">intake day</span>,
-          intake users will be able to issue vouchers to clients. Intake typically
-          begins one day prior to the actual check-in, and may continue up until
-          the day immediately before the check-out day.
+          intake users will be able to issue vouchers to clients. Intake
+          typically begins one day prior to the actual check-in, and may
+          continue up until the day immediately before the check-out day.
         </p>
       </div>
-      <div clsassName="mt-4">
+      <div className="mt-4">
         <IntakeDatePicker
           selectedDays={props.intakeDates}
           stayDates={props.stayDates}
