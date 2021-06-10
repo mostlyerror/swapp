26fbd678b12c79c274eb8fca1cf324f9bf8e0c2f
Benjamin Poon <benjamintpoon@gmail.com>
2021-06-10 11:13:03 -0600

    make swap period tests green

--- test/factories/swaps.rb
+++ test/factories/swaps.rb
@@ -15,6 +15,7 @@ FactoryBot.define do
       end_date { Date.current - 4 }
       intake_start_date { self.start_date - 1 }
       intake_end_date { self.end_date - 1 }
+      intake_dates { (self.intake_start_date..self.intake_end_date).to_a }
     end
 
     trait :current do
@@ -22,6 +23,7 @@ FactoryBot.define do
       end_date { Date.current.tomorrow }
       intake_start_date { self.start_date - 1 }
       intake_end_date { self.end_date - 1 }
+      intake_dates { (self.intake_start_date..self.intake_end_date).to_a }
     end
 
     trait :tomorrow do
@@ -29,6 +31,7 @@ FactoryBot.define do
       end_date { Date.current + 3 }
       intake_start_date { self.start_date - 1 }
       intake_end_date { self.end_date - 1 }
+      intake_dates { (self.intake_start_date..self.intake_end_date).to_a }
     end
 
     trait :future do
@@ -36,6 +39,7 @@ FactoryBot.define do
       end_date { Date.current + 8 }
       intake_start_date { self.start_date - 1 }
       intake_end_date { self.end_date - 1 }
+      intake_dates { (self.intake_start_date..self.intake_end_date).to_a }
     end
   end
 end
