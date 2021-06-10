c4c67942d9532e055b4ce67e073cba32dab8f03c
Ben Poon <benjamintpoon@gmail.com>
2021-06-10 00:34:36 -0600

    Awesome admin intake dates list (#123)
    * use toLocaleDateString() instead of dayjs
    
    * remove unused import
    
    * display list of selected intake dates interactively
    
    * linebreak in selected intake date list
    
    * intakeDates can be invalid again after being flipped to true
    
    * increase outline button text size to match non-outlined
    
    * add easing function to animation timing

--- app/javascript/components/SwapWizard/SwapWizard.js
+++ app/javascript/components/SwapWizard/SwapWizard.js
@@ -7,7 +7,6 @@ import { Step2 } from './Step2'
 import { Step3 } from './Step3'
 import { Step4 } from './Step4'
 import { Step5 } from './Step5'
-import dayjs from 'dayjs'
 import { sortDatesArray } from '../utils'
 
 class SwapWizard extends React.Component {
@@ -43,11 +42,10 @@ class SwapWizard extends React.Component {
   }
 
   handleIntakeDatesChange = (intakeDates) => {
-    this.setState({ intakeDates: sortDatesArray(intakeDates) })
-
-    if (intakeDates.length >= 1) {
-      this.setState({ intakeDatesValid: true })
-    }
+    this.setState({
+      intakeDates: sortDatesArray(intakeDates),
+      intakeDatesValid: intakeDates.length >= 1,
+    })
   }
 
   handleSubmit = (event) => {
