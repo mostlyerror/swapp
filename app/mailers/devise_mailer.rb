1ed226bd3c41b7f965fc099ca85ef16266d49cff
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-06-17 15:18:24 -0700

    changes mailer

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,8 +1,8 @@
 class MyMailer < Devise::Mailer   
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
-  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+  default template_path: './views/devise/mailer' # to make sure that your mailer uses the devise views
   # If there is an object in your application that returns a contact email, you can use it as follows
   # Note that Devise passes a Devise::Mailer object to your proc, hence the parameter throwaway (*).
-  default from: ->(*) { Class.instance.email_address }
+  default from: ->(*) { 'bjohnson@codeforamerica.org' }
 end
