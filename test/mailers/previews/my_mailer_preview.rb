4dd916bd5c473ef1f5ca2675f4e570f2f753f3f0
ftang-cfa <ftang@codeforamerica.org>
2021-06-18 19:09:12 -0700

    rename devise_mailer.rb to my_mailer.rb, add my_mailer_preview.rb

---
+++ test/mailers/previews/my_mailer_preview.rb
@@ -0,0 +1,16 @@
+# Preview all emails at http://localhost:3000/rails/mailers/my_mailer
+
+class MyMailerPreview < ActionMailer::Preview
+
+    def confirmation_instructions
+      MyMailer.confirmation_instructions(User.first, "faketoken", {})
+    end
+  
+    def reset_password_instructions
+      MyMailer.reset_password_instructions(User.first, "faketoken", {})
+    end
+  
+    def unlock_instructions
+      MyMailer.unlock_instructions(User.first, "faketoken", {})
+    end
+  end
