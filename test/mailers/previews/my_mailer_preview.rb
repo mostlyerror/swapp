ccbd40dccdb011e69c43198dd911124aeb776e8c
ftang-cfa <ftang@codeforamerica.org>
2021-06-19 00:08:51 -0700

    update language in mailer views

--- test/mailers/previews/my_mailer_preview.rb
+++ test/mailers/previews/my_mailer_preview.rb
@@ -5,12 +5,20 @@ class MyMailerPreview < ActionMailer::Preview
     def confirmation_instructions
       MyMailer.confirmation_instructions(User.first, "faketoken", {})
     end
-  
+
+    def email_changed
+      MyMailer.email_changed(User.first, "faketoken", {})
+    end    
+
+    def password_change
+      MyMailer.password_change(User.first, "faketoken", {})
+    end    
+
     def reset_password_instructions
       MyMailer.reset_password_instructions(User.first, "faketoken", {})
-    end
+    end 
   
-    def unlock_instructions
-      MyMailer.unlock_instructions(User.first, "faketoken", {})
-    end
+    # def unlock_instructions
+    #   MyMailer.unlock_instructions(User.first, "faketoken", {})
+    # end
   end
