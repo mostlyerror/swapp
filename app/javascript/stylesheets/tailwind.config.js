e54238763d9e44202277a5c70635cc61f9e7395f
Benjamin Poon <benjamintpoon@gmail.com>
2021-06-25 00:05:01 -0600

    extend tailwind with 128 (32rem) spacing utility classes

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,45 +1,46 @@
 module.exports = {
   purge: [
-    "./app/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/javascript/**/*.vue",
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
-        sm: "425px",
+        sm: '425px',
       },
       borderColor: {
-        "gray-350": "rgb(107, 114, 128)",
+        'gray-350': 'rgb(107, 114, 128)',
       },
       fontSize: {
-        "3.5xl": "2rem",
-        "4.5xl": "2.75rem",
-        "5.5xl": "3.5rem",
+        '3.5xl': '2rem',
+        '4.5xl': '2.75rem',
+        '5.5xl': '3.5rem',
       },
       spacing: {
-        18: "4.5rem",
-        28: "7rem",
+        18: '4.5rem',
+        28: '7rem',
+        128: '32rem',
       },
       flex: {
-        2: "2 2 0%",
+        2: '2 2 0%',
       },
       outline: {
-        black: ["2px solid black", "2px"],
-        gray: ["2px solid #d3d3d3", "2px"],
+        black: ['2px solid black', '2px'],
+        gray: ['2px solid #d3d3d3', '2px'],
       },
       inset: {
-        128: "32rem",
-        "1/5": "20%",
-        "1/8": "12.5%",
-        "1/10": "10%",
+        128: '32rem',
+        '1/5': '20%',
+        '1/8': '12.5%',
+        '1/10': '10%',
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [require('@tailwindcss/forms')],
+}
