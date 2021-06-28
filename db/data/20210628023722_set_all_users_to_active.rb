72a97cbe735b3d53fb82d206cee1418323ba2e84
Benjamin Poon <benjamintpoon@gmail.com>
2021-06-27 21:25:02 -0600

    only update users.active if not set

--- db/data/20210628023722_set_all_users_to_active.rb
+++ db/data/20210628023722_set_all_users_to_active.rb
@@ -1,7 +1,9 @@
 class SetAllUsersToActive < ActiveRecord::Migration[6.0]
   def up
     User.all.each do |user|
-      user.update(active: true)
+      if user.active.nil?
+        user.update(active: true)
+      end
     end
   end
 
