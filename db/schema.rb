1b300b9db4f40a30e4078ca075e475fcff8595e9
Benjamin Poon <benjamintpoon@gmail.com>
2021-10-14 17:52:19 -0500

    artifacts after re-installing and re-migrating

--- db/schema.rb
+++ db/schema.rb
@@ -48,9 +48,6 @@ ActiveRecord::Schema.define(version: 2021_06_28_015817) do
     t.boolean "force_intake", default: false
   end
 
-  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
-  end
-
   create_table "hotels", force: :cascade do |t|
     t.string "name", null: false
     t.json "address"
@@ -74,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_06_28_015817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "reporter_id"
-    t.integer "hotel_id", null: false
+    t.integer "hotel_id"
     t.boolean "red_flag", default: false
     t.index ["client_id"], name: "index_incident_reports_on_client_id"
     t.index ["reporter_id"], name: "index_incident_reports_on_reporter_id"
@@ -169,11 +166,7 @@ ActiveRecord::Schema.define(version: 2021_06_28_015817) do
     t.boolean "hotel_user", default: false, null: false
     t.boolean "intake_user", default: false, null: false
     t.boolean "show_swap_panel", default: true
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.boolean "active"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.boolean "active", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
