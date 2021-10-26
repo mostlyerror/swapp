e9bf270624a2aef1217c2342e48f90344b9791d8
Benjamin Poon <benjamintpoon@gmail.com>
2021-10-26 15:27:32 -0500

    make migration reversible

--- db/migrate/20211026202417_add_default_for_hotel_pet_friendly_column.rb
+++ db/migrate/20211026202417_add_default_for_hotel_pet_friendly_column.rb
@@ -1,5 +1,11 @@
 class AddDefaultForHotelPetFriendlyColumn < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    Hotel.where(pet_friendly: nil).update_all(pet_friendly: false)
+
     change_column :hotels, :pet_friendly, :boolean, default: false
+  end
+
+  def down
+    change_column :hotels, :pet_friendly, :boolean, default: nil
   end
 end
