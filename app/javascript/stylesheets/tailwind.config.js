6d1d2205ed383dc205df28df37d3f87faac86700
Benjamin Poon <benjamintpoon@gmail.com>
2021-10-27 22:06:13 -0500

    config defaults for tailwind container util

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      DEFAULT: '1rem',
+      sm: '2rem',
+      lg: '4rem',
+      xl: '5rem',
+      '2xl': '6rem',
+    },
     extend: {
       screens: {
         sm: '425px',
