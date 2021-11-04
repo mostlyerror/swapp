4d0b96b141f25ba57d4d7cd9cca5ce1abe6c14ab
Benjamin Poon <benjamintpoon@gmail.com>
2021-11-04 01:30:41 -0500

    ad responsible_id metadata logging to admin actions audit log

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,8 @@
 class Admin::BaseController < ApplicationController
   layout 'admin/admin'
+  around_action :use_logidze_responsible, only: %i[create update]
+
+  def use_logidze_responsible(&block)
+    Logidze.with_responsible(current_user&.id, &block)
+  end
 end
