f2b3032c006ebf002897ec614eaf1715922898a8
Benjamin Poon <benjamintpoon@gmail.com>
2021-11-16 22:30:50 -0600

    fix failing tests

--- test/factories/contacts.rb
+++ test/factories/contacts.rb
@@ -15,5 +15,11 @@
 #
 FactoryBot.define do
   factory :contact do
+    email { FFaker::Internet.email }
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    phone { FFaker::PhoneNumber.phone_number }
+    preferred_contact_method { %w(text email phone).sample }
+    title { %w(front_desk general_manager).sample }
   end
 end
