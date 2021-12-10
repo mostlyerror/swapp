7bb406d3243fe328752b718d58c7592782082767
Benjamin Poon <benjamintpoon@gmail.com>
2021-12-10 17:05:27 -0600

    fix voucher detail page crash when incident date not set

--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -1,13 +1,16 @@
 module DatesHelper
   def humanize_date(dt)
+    return "" if dt.nil?
     dt.strftime("%A, %b #{dt.day.ordinalize}")
   end
 
   def humanize_date_with_year(dt)
+    return "" if dt.nil?
     dt.strftime("%A, %b #{dt.day.ordinalize}, %Y")
   end
 
   def humanize_date_short(dt)
+    return "" if dt.nil?
     dt.strftime("%a, %b #{dt.day.ordinalize}")
   end
 end
