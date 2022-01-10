5a3e557b1044182335b7dd782ebf7701a0ac2c5b
Benjamin Poon <benjamintpoon@gmail.com>
2022-01-09 20:24:13 -0700

    add test for defaulting nil-received vehicle value to boolean (false)

--- test/controllers/vouchers_controller_test.rb
+++ test/controllers/vouchers_controller_test.rb
@@ -22,7 +22,7 @@
 #  created_at               :datetime         not null
 #  updated_at               :datetime         not null
 #
-require 'test_helper'
+require "test_helper"
 
 class VouchersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
@@ -39,9 +39,9 @@ class VouchersControllerTest < ActionDispatch::IntegrationTest
 
   teardown { Timecop.return }
 
-  test 'should create short_intake and voucher' do
-    assert_difference('ShortIntake.count') do
-      assert_difference('Voucher.count') do
+  test "should create short_intake and voucher" do
+    assert_difference("ShortIntake.count") do
+      assert_difference("Voucher.count") do
         post vouchers_url, params: create_params
       end
     end
@@ -49,28 +49,39 @@ class VouchersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to voucher_created_url(Voucher.last)
   end
 
+  test "should default vehicle to false if not received" do
+    without_vehicle = create_params.dup
+    without_vehicle["voucher"]["short_intake"]["vehicle"] = nil
+
+    post vouchers_url, params: without_vehicle
+
+    assert_redirected_to voucher_created_url(Voucher.last)
+    assert_equal false, ShortIntake.last.vehicle
+  end
+
   private
 
   def create_params
     {
-      'voucher' => {
-        'short_intake' => {
-          'where_did_you_sleep_last_night' => 'In own home',
-          'what_city_did_you_sleep_in_last_night' => 'qwer',
-          'why_not_shelter' => ['Safety concerns', '0'],
-          'pets' => 'Yes. A dog that WILL NOT stay with me in the hotel',
-          'identification' => 'Yes, SWAP ID',
+      "voucher" => {
+        "short_intake" => {
+          "where_did_you_sleep_last_night" => "In own home",
+          "what_city_did_you_sleep_in_last_night" => "qwer",
+          "why_not_shelter" => ["Safety concerns", "0"],
+          "pets" => "Yes. A dog that WILL NOT stay with me in the hotel",
+          "vehicle" => "Yes",
+          "identification" => "Yes, SWAP ID"
         },
-        'check_in' => @swap.start_date,
-        'check_out' => @swap.end_date,
-        'hotel_id' => @hotel.id,
-        'client' => {
-          'id' => @client.id,
-          'phone_number' => '',
-          'email' => '',
-        },
+        "check_in" => @swap.start_date,
+        "check_out" => @swap.end_date,
+        "hotel_id" => @hotel.id,
+        "client" => {
+          "id" => @client.id,
+          "phone_number" => "",
+          "email" => ""
+        }
       },
-      'commit' => 'Create Voucher',
+      "commit" => "Create Voucher"
     }
   end
 end
