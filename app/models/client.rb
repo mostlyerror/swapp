1c81a024b0096d04122e4284dd88bbb46d0c7686
Ben Poon <benjamintpoon@gmail.com>
2022-01-30 22:37:22 -0600

    create voucher button reacts to full/partial ban, display partial ban (#195)
    hotels on client show page

--- app/models/client.rb
+++ app/models/client.rb
@@ -25,36 +25,36 @@
 class Client < ApplicationRecord
   has_logidze
   include PgSearch::Model
-  pg_search_scope :that_sounds_like, against: [:first_name, :last_name], using: :dmetaphone
+  pg_search_scope :that_sounds_like,
+                  against: %i[first_name last_name],
+                  using: :dmetaphone
 
   GENDER = [
-    "Female",
-    "Male",
-    "Trans Female (MTF)",
-    "Trans Male (FTM)",
-    "Gender Non-Conforming",
+    'Female',
+    'Male',
+    'Trans Female (MTF)',
+    'Trans Male (FTM)',
+    'Gender Non-Conforming',
     "Client Doesn't Know",
-    "Client Refused"
+    'Client Refused',
   ]
 
   RACE = [
-    "American Indian or Alaska Native",
-    "Asian",
-    "Black or African American",
-    "Native Hawaiian or other Pacific Islander",
-    "White"
+    'American Indian or Alaska Native',
+    'Asian',
+    'Black or African American',
+    'Native Hawaiian or other Pacific Islander',
+    'White',
   ]
 
-  ETHNICITY = [
-    "Not Hispanic or Latino",
-    "Hispanic or Latino"
-  ]
+  ETHNICITY = ['Not Hispanic or Latino', 'Hispanic or Latino']
 
   auto_strip_attributes :first_name, :last_name, :race, :phone_number, :email
 
   validates :first_name, :last_name, :date_of_birth, presence: true
   validates :gender, inclusion: { in: Client::GENDER, allow_nil: true }
   validates :ethnicity, inclusion: { in: Client::ETHNICITY, allow_nil: true }
+
   # validate :accepted_race_values
   # validates :phone_number, phone: { possible: true, allow_nil: true }
   # validates_format_of :email, with: Devise.email_regexp, allow_nil: true
@@ -63,13 +63,11 @@ class Client < ApplicationRecord
   has_many :short_intakes
   has_many :vouchers
   has_many :incident_reports
-  has_many :red_flags, class_name: "RedFlag", table_name: :red_flags
+  has_many :red_flags, class_name: 'RedFlag', table_name: :red_flags
   has_many :flagged_hotels, through: :red_flags, source: :hotel
 
   # Callbacks
-  before_save do
-    self.race = self&.race&.reject { |r| r == "0" }
-  end
+  before_save { self.race = self&.race&.reject { |r| r == '0' } }
 
   def name
     "#{first_name} #{last_name}"
@@ -79,17 +77,22 @@ class Client < ApplicationRecord
     vouchers.flat_map(&:guests).uniq
   end
 
+  def ban!
+    update(banned: true)
+  end
+
   def ban_at!(hotel)
     return false if existing_flag = red_flags.find_by(hotel: hotel)
     red_flags.create(hotel: hotel)
   end
 
-  def ban!
-    update(banned: true)
-  end
-
   def unban!
     update(banned: false)
+  end
+
+  def unban_at!(hotel)
+    return false unless existing_flag = red_flags.find_by(hotel: hotel)
+    existing_flag.destroy!
   end
 
   def partial_ban?
