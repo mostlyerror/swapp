9d5fa995331391d8cf3ee3d688b74c1c348012ee
Ben Poon <benjamintpoon@gmail.com>
2022-10-23 16:08:05 -0600

    handle create hotel request

--- app/controllers/admin/hotels_controller.rb
+++ app/controllers/admin/hotels_controller.rb
@@ -2,14 +2,36 @@ class Admin::HotelsController < Admin::BaseController
   def index
     respond_to do |format|
       format.json do
-        render json: Hotel.all
+        render json: Hotel.with_deleted.all
       end
 
       format.csv do
         ts = Time.zone.now.strftime("%Y%m%dT%H%M")
-        send_data Hotel.to_csv, filename: "hotels-#{ts}.csv"
+        send_data Hotel.with_deletd.to_csv, filename: "hotels-#{ts}.csv"
       end
     end
+  end
+
+  # POST /admin/hotels
+  # POST /admin/hotels.json
+  def create
+    address = {
+      street: params["street_address"],
+      city: params["city"],
+      zip: params["zip"]
+    }
+
+    @hotel = Hotel.new(hotel_params.merge(address: address))
+
+    if @hotel.save
+      render json: @hotel, status: :created
+    else
+      render json: @hotel.errors, status: :unprocessable_entity
+    end
+  end
+
+  def hotel_params
+    params.require(:hotel).permit(%i[name phone street_address city zip])
   end
 
   def importer
