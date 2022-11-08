c774545326d0d4dea901b452db6ec00759dd0466
Ben Poon <benjamintpoon@gmail.com>
2022-11-08 00:06:53 -0600

    update default from for single sender verification

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "bjohnson@codeforamerica.org"
+  # single sender verification
+  # https://docs.sendgrid.com/ui/sending-email/sender-verification
+  default from: "benjamintpoon@gmail.com"
   layout "mailer"
 end
