8bc799fc995d51986afaaec9f6f39539ba7f9b88
Ben Poon <benjamintpoon@gmail.com>
2023-04-21 11:33:38 -0500

    require csv gem at top-level of application to make available for all controllers

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# https://github.com/mostlyerror/swapp/issues/267
+require 'csv'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
