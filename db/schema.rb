0417421b457b2989029065f1bcd78a1759729c3d
Ben Poon <benjamintpoon@gmail.com>
2023-05-30 23:32:05 -0500

    migration for confirmable columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_02_164513) do
+ActiveRecord::Schema.define(version: 2023_05_30_223121) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "hstore"
@@ -242,6 +243,11 @@ ActiveRecord::Schema.define(version: 2023_05_02_164513) do
     t.boolean "show_swap_panel", default: true
     t.boolean "active", default: true
     t.jsonb "log_data"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
