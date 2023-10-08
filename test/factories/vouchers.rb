8a2a5f9968c1112efffb1ba366a37fb4fb75e213
Ben Poon <benjamintpoon@gmail.com>
2023-10-08 15:09:44 -0500

    update voucher factory to always create required short_intake record

--- test/factories/vouchers.rb
+++ test/factories/vouchers.rb
@@ -1,20 +1,29 @@
 FactoryBot.define do
   factory :voucher do
     client
+    short_intake
     association :issuer, factory: :user
     hotel
     swap { Swap.current || create(:swap, :tomorrow) }
     check_in { swap.start_date }
     check_out { swap.end_date }
-    notes do
-      ([true, false].sample && FFaker::HipsterIpsum.words(20)) || nil
-    end
+    notes { ([true, false].sample && FFaker::HipsterIpsum.words(20)) || nil }
     voided_at { nil }
     voided_by { nil }
 
     after(:create) do |voucher|
-      create(:intake, client: voucher.client, user: voucher.issuer, swap: voucher.swap)
-      create(:short_intake, client: voucher.client, user: voucher.issuer, swap: voucher.swap)
+      create(
+        :intake,
+        client: voucher.client,
+        user: voucher.issuer,
+        swap: voucher.swap,
+      )
+      create(
+        :short_intake,
+        client: voucher.client,
+        user: voucher.issuer,
+        swap: voucher.swap,
+      )
     end
   end
 end
